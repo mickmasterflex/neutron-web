a09f05c1596bee56857a323293046e5e92148129
Kevin Bigelow <kbigelow5@gmail.com>
2020-03-12 14:43:37 -0600

    composing some buttons from utility classes, updating requirements

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="h-screen flex flex-row p-5">
+    <div id="nav" class="w-1/12 h-screen flex flex-col items-center justify-center">
+      <router-link to="/" class="btn btn-red">Home</router-link>
+      <router-link to="/about" class="btn btn-proooooooton">About</router-link>
     </div>
-    <router-view/>
+    <div class="w-11/12 rounded-lg bg-gray-100">
+      <router-view/>
+    </div>
   </div>
 </template>
