f394d96acc68a28af8a61594b29a7fa8d6b87e91
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-04-03 14:32:40 -0600

    added buyer list

---
+++ src/components/contracts/buyer/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <table v-if="contracts" class="w-full">
+      <tr class="th-row">
+        <th class="th">Name</th>
+        <th class="th">Parent</th>
+      </tr>
+      <tr v-for="contract in contracts" :key="contract.id">
+        <td class="td">
+          <router-link :to="{name: 'BuyerContract', params: {contract:contract.id}}" class="underline text-blue-500">{{contract.name}}</router-link>
+        </td>
+        <td>
+          {{ contract.parent}}
+        </td>
+      </tr>
+    </table>
+    <div v-else>
+      ...Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+props: ['contracts']
+}
+</script>
