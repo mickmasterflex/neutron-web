fb714c746f7a7bf69ef212282afff57e22385c99
Kevin Bigelow <kbigelow5@gmail.com>
2020-04-03 11:27:17 -0600

    adding delete partner contract

--- src/views/relationships/contracts/partner.vue
+++ src/views/relationships/contracts/partner.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-gray-900 rounded-lg w-full p-8 grid grid-cols-1 lg:grid-cols-2 items-center">
-    <h1 class="h1 text-white">{{contract.name}}</h1>
-    <div>
-      <stat-card v-if="contract.parent" v-bind:data="contract.parent" v-bind:title="`Parent`" v-bind:color="`teal`"></stat-card>
-      <stat-card v-bind:data="contract.client" v-bind:title="`Client`" v-bind:color="`teal`"></stat-card>
+  <div>
+    <div class="bg-gray-900 rounded-lg w-full p-8 grid grid-cols-1 lg:grid-cols-2 items-center">
+      <h1 class="h1 text-white">{{contract.name}}</h1>
+      <div>
+        <stat-card v-if="contract.parent" v-bind:data="contract.parent" v-bind:title="`Parent`" v-bind:color="`teal`"></stat-card>
+        <stat-card v-bind:data="contract.client" v-bind:title="`Client`" v-bind:color="`teal`"></stat-card>
+      </div>
     </div>
+    <h3 class="h3 mt-5 mb-2">Partner Contracts</h3>
+    <delete-partner-contract :client="contract.client" :id="id"></delete-partner-contract>
   </div>
 </template>
 
 <script>
-import axios from '@/axios'
+import axios from '../../../axios'
+import deletePartnerContract from '../../../components/contracts/partner/delete'
 
 export default {
   data () {
@@ -24,6 +29,9 @@ export default {
     }
   },
   props: ['client', 'id'],
+  components: {
+    'delete-partner-contract': deletePartnerContract
+  },
   methods: {
     getContract () {
       axios
