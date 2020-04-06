48fff259698f3b2caf79b1735dfb9c08fb1df36e
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-04-06 14:16:34 -0600

    trying to fix errors in buyer contracts

---
+++ src/components/contracts/buyer/update.vue
@@ -0,0 +1,16 @@
+<template>
+  <form @submit.prevent="updateBuyerContract">
+    <text-field-stacked v-model="name" field_id="BuyerContractName" label="Name" :required="true"></text-field-stacked>
+    <select-field-stacked v-model="parent" :options="siblings" field_id="buyerParent" label="Parent"></select-field-stacked>
+    <button type="submit" class="btn btn-green mt-5">Submit</button>
+  </form>
+  </template>
+
+<script>
+import axios from '../../../axios'
+  expoer default {
+  data () {
+    return
+  }
+}
+</script>
