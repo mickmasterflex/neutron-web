31f3da80a415e29707a3d4972db220067f54f21c
Kevin Bigelow <kbigelow5@gmail.com>
2020-04-15 10:46:26 -0600

    create and delete mutations for partners

--- src/store/modules/partners.js
+++ src/store/modules/partners.js
@@ -26,30 +26,32 @@ const actions = {
   async createPartnerContract ({ commit }, partner) {
     await axios.post('/partners/', partner)
       .then(response => {
-        // commit('createPartner')
+        commit('createPartner', response.data)
       })
   },
   async updatePartnerContract ({ commit }, updatedPartner) {
     await axios.put(`/partners/${updatedPartner.id}/`, updatedPartner)
-      .then(response => {
+      .then(() => {
         commit('updatePartner', updatedPartner)
       })
   },
   async deletePartnerContract ({ commit }, id) {
     await axios.delete(`/partners/${id}/`)
-      .then(response => {
-        // commit('removePartner')
+      .then(() => {
+        commit('deletePartner', id)
       })
   }
 }
 const mutations = {
   setPartners: (state, partners) => (state.partners = partners),
+  createPartner: (state, partner) => state.partners.unshift(partner),
   updatePartner: (state, updatedPartner) => {
     const index = state.partners.findIndex(partner => partner.id === updatedPartner.id)
     if (index !== -1) {
       state.partners.splice(index, 1, updatedPartner)
     }
-  }
+  },
+  deletePartner: (state, id) => (state.partners = state.partners.filter(partner => partner.id !== id))
 }
 
 export default {
