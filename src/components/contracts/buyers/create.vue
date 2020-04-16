33193d9e3ade21f46418106054215b5084e41cc8
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-04-16 17:06:11 -0600

    updated/fixed Buyer files

--- src/components/contracts/buyers/create.vue
+++ src/components/contracts/buyers/create.vue
@@ -1,6 +1,6 @@
 <template>
   <validation-observer v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(createBuyerContract)">
+    <form @submit.prevent="handleSubmit(create)">
       <v-text-field v-model="name" rules="required" field_id="buyerName" field_label="Name" class="field-group"></v-text-field>
       <select-field v-model="parent" :options="buyerContracts" field_id="parent" field_label="Parent" class="field-group"></select-field>
       <button class="btn btn-green mt-5">Create Buyer Contract</button>
@@ -19,9 +19,16 @@ export default {
       output: ''
     }
   },
-  props: ['client', 'buyerContracts'],
+  props: {
+    client: {
+      type: Number
+    },
+    buyerContracts: {
+      type: Array
+    }
+  },
   methods: {
-    ...mapActions({ create: 'createBuyerContract' }),
+    ...mapActions({ create: 'createBuyer' }),
     submitForm () {
       this.create({
         name: this.name,
--- src/store/modules/buyers.js
+++ src/store/modules/buyers.js
@@ -1,18 +1,26 @@
 import axios from '@/axios'
 
 const state = {
-  buyers: []
+  buyers: [],
+  current_buyer: {}
 }
+
 const getters = {
   getAllBuyers: state => state.buyers,
+  getCurrentBuyer: state => state.current_buyer,
   getAllBuyersCount: (state) => {
     return state.buyers.length
   },
   getBuyersByClient: (state) => (clientId) => {
     return state.buyers.filter(buyer => buyer.client === clientId)
   },
-  getBuyerById: (state) => (id) => {
-    return state.buyers.find(buyer => buyer.id === id)
+  getUserSiblings: (state, getters) => {
+    const siblings = getters.getBuyersByClient(state.current_buyer.client)
+    const index = siblings.findIndex(user => user.id === state.current_buyer.id)
+    if (index !== -1) {
+      siblings.splice(index, 1)
+    }
+    return siblings
   }
 }
 
@@ -20,38 +28,46 @@ const actions = {
   async fetchBuyers ({ commit }) {
     await axios.get('/buyers/')
       .then(response => {
-        commit('setBuyers', response.data)
+        commit('SET_BUYERS', response.data)
       })
   },
-  async createBuyerContract ({ commit }, buyer) {
+  async fetchCurrentBuyer ({ commit }, id) {
+    await axios.get(`/buyers/${id}/`)
+      .then(response => {
+        commit('SET_CURRENT_BUYER', response.data)
+      })
+  },
+  async createBuyer ({ commit }, buyer) {
     await axios.post('/buyers/', buyer)
       .then(response => {
-        commit('createBuyers', response.data)
+        commit('ADD_BUYER', response.data)
       })
   },
-  async updateBuyerContract ({ commit }, updatedBuyer) {
+  async updateBuyer ({ commit }, updatedBuyer) {
     await axios.put(`/buyers/${updatedBuyer.id}/`, updatedBuyer)
       .then(() => {
-        commit('updateBuyers', updatedBuyer)
+        commit('UPDATE_BUYER', updatedBuyer)
+        commit('SET_CURRENT_BUYER', updatedBuyer)
       })
   },
-  async deleteBuyerContract ({ commit }, id) {
+  async deleteBuyer ({ commit }, id) {
     await axios.delete(`/buyers/${id}/`)
       .then(() => {
-        commit('deleteBuyers', id)
+        commit('REMOVE_BUYER', id)
       })
   }
 }
 const mutations = {
-  setBuyers: (state, buyers) => (state.buyers = buyers),
-  createBuyer: (state, buyer) => state.buyers.unshift(buyer),
-  updateBuyer: (state, updatedBuyer) => {
+  SET_BUYERS: (state, buyers) => (state.buyers = buyers),
+  SET_CURRENT_BUYER: (state, buyer) => (state.current_buyer = buyer),
+  ADD_BUYER: (state, buyer) => state.buyers.unshift(buyer),
+  UPDATE_BUYER: (state, updatedBuyer) => {
     const index = state.buyers.findIndex(buyer => buyer.id === updatedBuyer.id)
     if (index !== -1) {
       state.buyers.splice(index, 1, updatedBuyer)
     }
   },
-  deleteBuyer: (state, id) => (state.buyers = state.buyers.filter(buyer => buyer.id !== id))
+  REMOVE_BUYER: (state, id) => (state.buyers = state.buyers.filter(buyer => buyer.id !== id))
 }
 
 export default {
