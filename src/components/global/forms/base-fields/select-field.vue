72c0b3acdc4fcf3a144f6931ad9346ce26d02237
Kevin Bigelow <kbigelow5@gmail.com>
2020-04-29 14:54:16 -0600

    if option.id == value, select that option

--- src/components/global/forms/base-fields/select-field.vue
+++ src/components/global/forms/base-fields/select-field.vue
@@ -7,7 +7,7 @@
         class="appearance-none base-field"
         :id="$attrs.field_id">
         <option value="">----------</option>
-        <option v-for="option in $attrs.options" :key="option.id" :value="`${option.id}`">{{option.name}}</option>
+        <option :selected="option.id == value ? 'selected' : false" v-for="option in $attrs.options" :key="option.id" :value="`${option.id}`">{{option.name}}</option>
       </select>
       <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
