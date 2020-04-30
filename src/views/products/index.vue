b8f2aa9c8509d858eda1114fda6771439675088c
Kevin Bigelow <kbigelow5@gmail.com>
2020-04-30 14:51:36 -0600

    fixed products temporary links

--- src/views/products/index.vue
+++ src/views/products/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <ul class="flex-row">
-      <li class="active"><router-link :to="{name: 'Products'}">Product Mgmt</router-link></li>
-      <li><router-link :to="{name: 'FieldManagement'}">Field Mgmt</router-link></li>
+    <ul class="flex flex-row mb-3">
+      <li class="active"><router-link class="text-link" :to="{name: 'Products'}">Product Mgmt</router-link></li>
+      <li class="px-3"> | </li>
+      <li><router-link class="text-link" :to="{name: 'FieldManagement'}">Field Mgmt</router-link></li>
     </ul>
     <div class="hud">
       <h1 class="h1 text-white">Product Management</h1>
