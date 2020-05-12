b4a974af43d7a7cb4f029ad48b4936f2797b4bc9
Kevin Bigelow <kbigelow5@gmail.com>
2020-05-12 14:17:06 -0600

    importing base-field modules into base-fields index. updating relative paths

---
+++ src/store/modules/base-fields/index.js
@@ -0,0 +1,13 @@
+import baseTextFields from '@/store/modules/base-fields/base-text-fields'
+import baseOptionFields from '@/store/modules/base-fields/base-option-fields'
+import baseOptions from '@/store/modules/base-fields/base-options'
+
+const modules = {
+  baseTextFields,
+  baseOptionFields,
+  baseOptions
+}
+
+export default {
+  modules
+}
