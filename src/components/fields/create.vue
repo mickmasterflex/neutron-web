b4a4f9bd95407a5da2bcb73779c55f40435f9625
Kevin Bigelow <kbigelow5@gmail.com>
2020-05-14 11:10:37 -0600

    close create field form

--- src/components/fields/create.vue
+++ src/components/fields/create.vue
@@ -1,13 +1,16 @@
 <template>
-  <validation-observer v-slot="{ handleSubmit }" ref="form">
-    <form @submit.prevent="handleSubmit(submitForm)">
-    <v-text-field v-model="label" rules="required" field_label="Label" field_id="createFieldLabel" class="field-group"></v-text-field>
-    <text-field v-model="mapping" field_label="Mapping" field_id="createFieldMapping" class="field-group"></text-field>
-    <checkbox-single v-model="deliver" field_label="Deliver" field_id="createFieldDeliver" class="field-group"></checkbox-single>
-    <v-select-field v-model="base_field" rules="required" field_label="Base Field" :options="baseFields"></v-select-field>
-    <button type="submit" class="btn btn-green mt-5">Add Field</button>
-    </form>
-  </validation-observer>
+  <div>
+    <button class="btn btn-green" @click="showForm=true" v-show="!showForm">Add Field</button>
+    <validation-observer v-slot="{ handleSubmit }" ref="form" v-show="showForm">
+      <form @submit.prevent="handleSubmit(submitForm)">
+      <v-text-field v-model="label" rules="required" field_label="Label" field_id="createFieldLabel" class="field-group"></v-text-field>
+      <text-field v-model="mapping" field_label="Mapping" field_id="createFieldMapping" class="field-group"></text-field>
+      <checkbox-single v-model="deliver" field_label="Deliver" field_id="createFieldDeliver" class="field-group"></checkbox-single>
+      <v-select-field v-model="base_field" rules="required" field_label="Base Field" :options="baseFields"></v-select-field>
+      <button type="submit" class="btn btn-green mt-5">Add Field</button><span class="text-link" @click="showForm=false">Close</span>
+      </form>
+    </validation-observer>
+  </div>
 </template>
 
 <script>
@@ -16,6 +19,7 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data () {
     return {
+      showForm: false,
       label: '',
       mapping: '',
       deliver: '',
--- src/components/fields/field-management.vue
+++ src/components/fields/field-management.vue
@@ -2,8 +2,6 @@
   <div>
     <h3 class="h3 mt-5 mb-2">Form Fields</h3>
     <list-fields v-if="buyer.form" :form="buyer.form"></list-fields>
-
-    <h3 class="h3 mt-5 mb-2">Create Form Field</h3>
     <create-field v-if="buyer.form" :form="buyer.form.id"></create-field>
   </div>
 </template>
