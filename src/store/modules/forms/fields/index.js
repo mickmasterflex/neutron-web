292d0f3442f3a6cd997fd02b4350442079cf8598
Kevin Bigelow <kbigelow5@gmail.com>
2020-05-18 16:18:34 -0600

    moving field specfic state, getters, and mutatiosn into fields/index.js

--- src/store/modules/forms/fields/index.js
+++ src/store/modules/forms/fields/index.js
@@ -7,16 +7,27 @@ const modules = {
 }
 
 const state = {
+  current_form: [],
   current_field: null,
   show_create_field: false
 }
 
 const getters = {
+  getCurrentForm: state => state.current_form,
   getCurrentField: state => state.current_field,
   getShowCreateField: state => state.show_create_field
 }
 
 const mutations = {
+  SET_CURRENT_FORM: (state, form) => (state.current_form = form),
+  ADD_FIELD: (state, field) => state.current_form.fields.push(field),
+  UPDATE_FIELD: (state, updatedField) => {
+    const index = state.current_form.fields.findIndex(field => field.id === updatedField.id)
+    if (index !== -1) {
+      state.current_form.fields.splice(index, 1, updatedField)
+    }
+  },
+  REMOVE_FIELD: (state, id) => (state.current_form.fields = state.current_form.fields.filter(field => field.id !== id)),
   SET_CURRENT_FIELD: (state, field) => (state.current_field = field),
   RESET_CURRENT_FIELD: (state) => (state.current_field = null),
   TOGGLE_SHOW_CREATE_FIELD: (state, bool) => (state.show_create_field = bool)
