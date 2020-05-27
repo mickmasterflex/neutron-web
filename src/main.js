287c54ece189a55ea9524436e776e715e2b02fed
Kevin Bigelow <kbigelow5@gmail.com>
2020-05-27 13:21:21 -0600

    renaming global component dir to ui

--- src/main.js
+++ src/main.js
@@ -23,13 +23,13 @@ import simpleLayout from '@/layouts/simple-layout'
 import appLayout from '@/layouts/app-layout'
 
 /* Components */
-import baseModal from '@/components/global/modals/base'
-import statCard from '@/components/global/cards/stat-card'
-import checkboxSingle from '@/components/global/forms/base-fields/checkbox-single'
-import selectField from '@/components/global/forms/base-fields/select-field'
-import textField from '@/components/global/forms/base-fields/text-field'
-import vTextField from '@/components/global/forms/validation-fields/text-field'
-import vSelectField from '@/components/global/forms/validation-fields/select-field'
+import baseModal from '@/components/ui/modals/base'
+import statCard from '@/components/ui/cards/stat-card'
+import checkboxSingle from '@/components/ui/forms/base-fields/checkbox-single'
+import selectField from '@/components/ui/forms/base-fields/select-field'
+import textField from '@/components/ui/forms/base-fields/text-field'
+import vTextField from '@/components/ui/forms/validation-fields/text-field'
+import vSelectField from '@/components/ui/forms/validation-fields/select-field'
 
 /* -----------------------------------------------
   Authentication
