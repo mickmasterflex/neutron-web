9b91a2547ff91d1f4f8de6c63e949840fead4050
Kevin Bigelow <kbigelow5@gmail.com>
2020-06-01 15:10:01 -0600

    styles for field-draggable, sortable-drag, sortable-ghost, and adding basic label to top of list

--- src/components/forms/base-fields/option-fields/options/list.vue
+++ src/components/forms/base-fields/option-fields/options/list.vue
@@ -1,10 +1,13 @@
 <template>
-  <ul-draggable tag="ul" handle=".field-draggable" v-model="options" class="well" v-if="options">
-    <li v-for="(option, index) in options" :key="option.id" class="card card-sm mb-1 flex flex-row items-center justify-between">
-      <update-option :option="option" :newOrder="index + 1"></update-option>
-      <delete-option :option="option" class="mx-1"></delete-option>
-    </li>
-  </ul-draggable>
+  <div class="well">
+    <p>Order - Label - Value</p>
+    <ul-draggable tag="ul" handle=".field-draggable" v-model="options" v-if="options">
+      <li v-for="(option, index) in options" :key="option.id" class="card card-sm mb-1 flex flex-row items-center justify-between">
+        <update-option :option="option" :newOrder="index + 1"></update-option>
+        <delete-option :option="option" class="mx-1"></delete-option>
+      </li>
+    </ul-draggable>
+  </div>
 </template>
 
 <script>
