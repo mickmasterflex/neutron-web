9d7dfc7d0ed5f9c36f500c24a93c3c5114e955e7
Kevin Bigelow <kbigelow5@gmail.com>
2020-06-02 13:22:57 -0600

    switching name and label for base field table

--- src/components/forms/base-fields/list.vue
+++ src/components/forms/base-fields/list.vue
@@ -3,8 +3,8 @@
     <table v-if="baseFields" class="table">
       <thead>
         <tr class="th-row">
-          <th class="th">Name</th>
           <th class="th">Label</th>
+          <th class="th">Name</th>
           <th class="th">Description</th>
           <th class="th">Type</th>
           <th class="th"></th>
@@ -15,9 +15,9 @@
           <td class="td">
             <span class="text-link"
               @click="editBaseField(field.id, field.type)">
-              {{field.name}}</span>
+              {{field.label}}</span>
           </td>
-          <td class="td">{{field.label}}</td>
+          <td class="td">{{field.name}}</td>
           <td class="td">{{field.description}}</td>
           <td class="td">{{field.type}}</td>
           <td class="td flex flex-row justify-end">
