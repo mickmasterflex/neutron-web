ae3001c1b9361a136e0a74f26bbe4164a37f6dba
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-02 10:32:35 -0600

    setup templates for User Component

--- src/components/users/create.vue
+++ src/components/users/create.vue
@@ -1,16 +1,21 @@
 <template>
-  <validation-observer v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(submitForm)">
-      <v-text-field v-model="first_name" rules="required" field_id="first_name" field_label="First Name" class="field-group"></v-text-field>
-      <v-text-field v-model="last_name" rules="required" field_id="last_name" field_label="Last Name" class="field-group"></v-text-field>
-      <v-text-field v-model="email" rules="required|email" field_id="email" field_label="Email" field_type="email" class="field-group"></v-text-field>
-      <button type="submit" class="btn btn-green mt-5">Submit</button>
-    </form>
-  </validation-observer>
+  <modal-template :show="showModal">
+    <template v-slot:header>Create User</template>
+    <template v-slot:body>
+      <validation-observer v-slot="{ handleSubmit }">
+        <form @submit.prevent="handleSubmit(submitForm)">
+          <v-text-field v-model="first_name" rules="required" field_id="first_name" field_label="First Name" class="field-group"></v-text-field>
+          <v-text-field v-model="last_name" rules="required" field_id="last_name" field_label="Last Name" class="field-group"></v-text-field>
+          <v-text-field v-model="email" rules="required|email" field_id="email" field_label="Email" field_type="email" class="field-group"></v-text-field>
+          <button type="submit" class="btn btn-green mt-5">Submit</button>
+        </form>
+      </validation-observer>
+    </template>
+  </modal-template>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   data () {
@@ -29,6 +34,11 @@ export default {
         email: this.email
       })
     }
+  },
+  computed: {
+    ...mapGetters({
+      showModal: 'getShowCreateUserModal'
+    })
   }
 }
 </script>
