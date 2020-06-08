a710328d24699b7409c104c152210cea1772dfe7
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-08 10:48:10 -0600

    Set up templates for Partner Component

--- src/components/partners/create.vue
+++ src/components/partners/create.vue
@@ -1,12 +1,16 @@
 <template>
   <modal-template :show="showModal" @close="close">
-  <validation-observer v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(submitForm)">
+    <template v-slot:header>Create Partner Contract</template>
+    <template v-slot:body>
+  <validation-observer ref="form">
+    <form @submit.prevent="submitForm">
       <v-text-field v-model="name" rules="required" field_id="partnerName" field_label="Name" class="field-group"></v-text-field>
       <select-field v-model="parent" :options="partnerContracts" field_id="parent" field_label="Parent" class="field-group"></select-field>
       <button class="btn btn-green mt-5">Create Partner Contract</button>
     </form>
   </validation-observer>
+    </template>
+  </modal-template>
 </template>
 
 <script>
