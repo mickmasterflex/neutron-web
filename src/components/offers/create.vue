4ce4c0ae34a6d4fc3b9a437bee8daae8b0fe4166
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-12 13:53:15 -0600

    SetUp Templates for Offer Coponent

--- src/components/offers/create.vue
+++ src/components/offers/create.vue
@@ -1,11 +1,16 @@
 <template>
-  <validation-observer v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(submitForm)">
+  <modal-template :show="showModal">
+    <template v-slot:header>Create Offer Contract</template>
+    <template v-slot:body>
+  <validation-observer ref="form">
+    <form @submit.prevent="submitForm">
       <v-text-field v-model="name" rules="required" field_id="offerName" field_label="Offer Name" class="field-group"></v-text-field>
       <v-select-field v-model="product" rules="required" :options="products" field_id="product" field_label="Product" class="field-group"></v-select-field>
       <button type="submit" class="btn btn-green mt-5">Create Offer</button>
     </form>
   </validation-observer>
+    </template>
+  </modal-template>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
@@ -36,7 +41,8 @@ export default {
   computed: {
     ...mapGetters({
       products: 'getAllProducts'
-    })
+    }),
+    showModal: 'getShowCreateOfferModal'
   },
   created () {
     this.fetchProducts()
