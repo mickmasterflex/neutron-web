48cd9fd5e8527a48d44c2b167d6511041e05ff62
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-15 10:59:49 -0600

    Setup Templates For Client Component for Modal

--- src/components/clients/create.vue
+++ src/components/clients/create.vue
@@ -1,12 +1,15 @@
 <template>
   <modal-template :show="showModal">
-  <validation-observer v-slot="{ handleSubmit }">
-    <form @submit.prevent="handleSubmit(submitForm)">
+    <template v-slot:header>Create Client</template>
+    <template v-slot:body>
+  <validation-observer ref="form">
+    <form @submit.prevent="submitForm">
       <v-text-field v-model="name" rules="required" field_id="clientName" field_label="Client Name" class="field-group"></v-text-field>
       <v-text-field v-model="slug" rules="required|alpha_dash" field_id="clientSlug" field_label="Slug" class="field-group"></v-text-field>
       <button type="submit" class="btn btn-green mt-5">Submit</button>
     </form>
   </validation-observer>
+      </template>
   </modal-template>
 </template>
 
