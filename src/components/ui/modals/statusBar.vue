78b1e8bafa463ff0ccc038e4ce21000450b0f137
Kevin Bigelow <kbigelow5@gmail.com>
2020-06-17 17:28:15 -0600

    faster leave animation for statusBar

--- src/components/ui/modals/statusBar.vue
+++ src/components/ui/modals/statusBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <transition leave-active-class="animate__animated animate__fadeOut">
+  <transition leave-active-class="animate__animated animate__fadeOut animate__fast">
     <label v-show="show" :class="`bg-${color}-500 text-${color}-900 px-8 py-2 w-full block`">
-      <transition enter-active-class="animate__animated animate__headShake" leave-active-class="animate__animated animate__fadeOut">
+      <transition enter-active-class="animate__animated animate__headShake" leave-active-class="animate__animated animate__fadeOut animate__fast">
         <div class="copy" v-show="show">
           <font-awesome-icon v-if="icon" :icon="icon" class="mr-2"></font-awesome-icon>
           <span class="font-bold">{{copy}}</span>
