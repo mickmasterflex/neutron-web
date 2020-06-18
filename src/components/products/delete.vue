eb8efd3c63b27326cb1f1998490e098a83762da2
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-18 14:44:24 -0600

    delete and adding list products

--- src/components/products/delete.vue
+++ src/components/products/delete.vue
@@ -1,24 +1,24 @@
 <template>
-  <button class="btn btn-red" @click="runDelete">Delete Campaign</button>
+  <button class="btn btn-red" @click="runDelete">Delete Product</button>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 
-  export default {
-    props: {
-      campaign: {
-        type: Object
-      }
-    },
-    methods: {
-      ...mapActions({ delete: 'deleteCampaign' }),
-      runDelete () {
-        this.delete(this.campaign.id)
-          .then(() => {
-            this.$router.push({ name: 'PartnerContract', params: { id: this.campaign.contract, client: this.campaign.client } })
-          })
-      }
+export default {
+  props: {
+    product: {
+      type: Object
+    }
+  },
+  methods: {
+    ...mapActions({ delete: 'deleteProduct' }),
+    runDelete () {
+      this.delete(this.product.id)
+        .then(() => {
+          this.$router.push({ name: 'Product', params: { id: this.product.contract, client: this.product.client } })
+        })
     }
   }
+}
 </script>
