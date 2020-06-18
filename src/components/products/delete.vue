419e28ff3bfc198f81583e4eed9b6c622fa21388
Emily Jonaitis <emilyjonaitis@Admins-MBP.localdomain>
2020-06-18 11:38:49 -0600

    creating delete file for product

---
+++ src/components/products/delete.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-red" @click="runDelete">Delete Campaign</button>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+
+  export default {
+    props: {
+      campaign: {
+        type: Object
+      }
+    },
+    methods: {
+      ...mapActions({ delete: 'deleteCampaign' }),
+      runDelete () {
+        this.delete(this.campaign.id)
+          .then(() => {
+            this.$router.push({ name: 'PartnerContract', params: { id: this.campaign.contract, client: this.campaign.client } })
+          })
+      }
+    }
+  }
+</script>
