d25a54c550a22d371da0307c82ba5ea9b2cb7e5e
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-22 12:12:35 -0600

    adding delete.vue for brands

---
+++ src/components/brands/delete.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-red" @click="runDelete">Delete Brand</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: {
+    id: {
+      type: Number
+    }
+  },
+  methods: {
+    ...mapActions({ delete: 'deleteBrand' }),
+    runDelete () {
+      this.delete(this.id)
+        .then(() => {
+          this.$router.push({ name: 'Products' })
+        })
+    }
+  }
+}
+</script>
