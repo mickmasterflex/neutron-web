3a1ed9efad7f9850aa53caa715e239a35a71c44e
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-06-25 10:35:31 -0600

    delete.vue for campuses

---
+++ src/components/campuses/delete.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-red" @click="runDelete">Delete Campus</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: {
+    id: {
+      type: Number
+    }
+  },
+  methods: {
+    ...mapActions({ delete: 'deleteCampus' }),
+    runDelete () {
+      this.delete(this.id)
+        .then(() => {
+          this.$router.push({ name: 'Campuses' })
+        })
+    }
+  }
+}
+</script>
