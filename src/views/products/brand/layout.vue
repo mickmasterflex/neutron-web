20371db85471db2a4968792f16f9fafc3af27df5
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-07-09 13:24:01 -0600

    adding brand directory to products and creating layout.vue for brand

---
+++ src/views/products/brand/layout.vue
@@ -0,0 +1,49 @@
+<template>
+  <content-layout>
+    <template v-slot:hud>
+      <div class="hud">
+        <h1 class="h1 text-white">{{brand.name}}</h1>
+      </div>
+    </template>
+    <template v-slot:contentTabs>
+      <ul class="underscore-tabs">
+        <li class="underscore-tab" :class="contentTab === 'details' ? 'active' : ''">
+          <router-link :to="{name: 'BuyerContract', params: {id:id}}">Brand Details</router-link>
+        </li>
+        <li class="underscore-tab" :class="contentTab === 'offers' ? 'active' : ''">
+          <router-link :to="{name: 'BuyerContractOffers', params: {id:id}}">Campuses</router-link>
+        </li>
+      </ul>
+    </template>
+    <template v-slot:content>
+      <slot name="content"/>
+    </template>
+  </content-layout>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  props: {
+    id: Number,
+    contentTab: {
+      type: String,
+      default: 'details'
+    }
+  },
+  methods: {
+    ...mapActions({
+      fetchCurrentBrand: 'fetchCurrentBrand'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      brand: 'getCurrentBrand'
+    })
+  },
+  created () {
+    this.fetchCurrentBrand(this.id)
+  }
+}
+</script>
