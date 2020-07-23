fece597e239fa49dc611f169978cf4f08405a4f7
Kevin Bigelow <kbigelow5@gmail.com>
2020-07-23 15:41:55 -0600

    removing rounded class

--- src/components/ui/status-bar/default.vue
+++ src/components/ui/status-bar/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <label v-show="show" :class="`bg-${color}-500 text-${color}-900 px-4 py-2 w-full block rounded-full`">
+  <label v-show="show" :class="`bg-${color}-500 text-${color}-900 px-4 py-2 w-full block`">
     <transition enter-active-class="animate__animated animate__headShake">
       <div class="copy" v-show="show">
         <font-awesome-icon v-if="icon" :icon="icon" class="mr-2"></font-awesome-icon>
