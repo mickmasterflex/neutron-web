cf3f180fa53080278ee2dd9df8352599e04dc56d
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-04 11:22:46 -0600

    degree level working with create and update

--- src/components/education-products/create.vue
+++ src/components/education-products/create.vue
@@ -8,7 +8,7 @@
           <textarea-field v-model="short_description" field_id="short_description" field_label="Short Description"></textarea-field>
           <textarea-field v-model="description" field_id="description" field_label="Description"></textarea-field>
           <select-field v-model="area_of_interest" :options="interestOptions" field_id="area_of_interest" field_label="Area Of Interest"></select-field>
-          <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degreeLevel" field_label="Degree Level"></select-field>
+          <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degree_level" field_label="Degree Level"></select-field>
         </form>
       </validation-observer>
     </template>
@@ -99,6 +99,7 @@ export default {
             short_description: this.short_description,
             description: this.description,
             area_of_interest: this.area_of_interest,
+            degree_level: this.degree_level,
             product_group: this.campusId
           }).then(() => {
             this.close()
--- src/components/education-products/update.vue
+++ src/components/education-products/update.vue
@@ -5,7 +5,7 @@
       <textarea-field v-model="short_description" field_id="short_description" field_label="Short Description"></textarea-field>
       <textarea-field v-model="description" field_id="description" field_label="Description"></textarea-field>
       <select-field v-model="area_of_interest" :options="interestOptions" field_id="area_of_interest" field_label="Area Of Interest"></select-field>
-      <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degreeLevel" field_label="Degree Level"></select-field>
+      <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degree_level" field_label="Degree Level"></select-field>
       <button type="submit" class="btn btn-green mt-5">Submit</button>
     </form>
   </validation-observer>
@@ -72,6 +72,7 @@ export default {
         short_description: this.short_description,
         description: this.description,
         area_of_interest: this.area_of_interest,
+        degree_level: this.degree_level,
         product_group: this.product_group,
         id: this.educationProduct.id
       })
