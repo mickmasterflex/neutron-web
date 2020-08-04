8ba64709ff2066a4c47554c973a7c0d83b8c0e75
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-04 15:37:56 -0600

    moved update component and logic from list.vue to field-management.vue

--- src/components/forms/fields/list.vue
+++ src/components/forms/fields/list.vue
@@ -21,71 +21,29 @@
       </ul-draggable>
     </div>
     <table-empty-state v-else heading="No Fields" copy="Use the 'Add Field' button to start cloning base fields for this contract."></table-empty-state>
-    <component :is="update_component" :field="current_field"></component>
   </div>
 </template>
 
 <script>
-import updateTextField from '@/components/forms/fields/text-fields/update'
-import updateOptionField from '@/components/forms/fields/option-fields/update'
 import fetchCurrentField from '@/components/forms/fields/fetch-current-field'
 import deleteField from '@/components/forms/fields/delete'
 import fieldListItem from '@/components/forms/fields/list-item'
 import draggable from 'vuedraggable'
 import { dragOptions } from '@/mixins/dragOptions'
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
-  data () {
-    return {
-      current_field_id: {
-        default: null,
-        type: Number
-      },
-      update_component: null
-    }
-  },
   components: {
-    updateTextField,
-    updateOptionField,
     'delete-field': deleteField,
     'fetch-current-field': fetchCurrentField,
     'field-list-item': fieldListItem,
     'ul-draggable': draggable
   },
-  methods: {
-    ...mapMutations({
-      showUpdateTextFieldModal: 'SHOW_UPDATE_TEXT_FIELD_MODAL',
-      showUpdateOptionFieldModal: 'SHOW_UPDATE_OPTION_FIELD_MODAL'
-    }),
-    async setUpdateComponent (component) {
-      this.update_component = component
-    }
-  },
   mixins: [dragOptions],
   computed: {
     ...mapGetters({
-      current_field: 'getCurrentField',
       form: 'getCurrentForm'
     })
-  },
-  watch: {
-    current_field () {
-      if (this.current_field) {
-        this.current_field_id = this.current_field.id
-        if (this.current_field.type === 'select' || this.current_field.type === 'radio') {
-          this.setUpdateComponent(updateOptionField).then(() => {
-            this.showUpdateOptionFieldModal()
-          })
-        } else if (this.current_field.type === 'text' || this.current_field.type === 'textarea') {
-          this.setUpdateComponent(updateTextField).then(() => {
-            this.showUpdateTextFieldModal()
-          })
-        }
-      } else {
-        this.current_field_id = null
-      }
-    }
   }
 }
 </script>
