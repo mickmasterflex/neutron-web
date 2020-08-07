1b9a51799ac40be0b4ebf8fa69794b14a8a00906
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-07 13:31:04 -0600

    slideIn for clone field form

--- src/components/forms/fields/create.vue
+++ src/components/forms/fields/create.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <button class="btn btn-turquoise" @click="showForm()" v-show="!formVisible"><font-awesome-icon icon="plus"></font-awesome-icon> Add Field</button>
-    <div class="flex flex-row items-start" v-show="formVisible">
-      <validation-observer ref="form">
-        <form @submit.prevent="submitForm" class="form-horizontal form-horizontal-slim">
-          <v-select-field rules="required" field_class="field-tall" v-model="baseField" field_label="Base Field" field_id="baseFieldSelectToClone" :options="baseFields"></v-select-field>
-        </form>
-      </validation-observer>
-      <button @click="submitForm()" class="btn btn-green ml-2"><font-awesome-icon icon="clone"></font-awesome-icon> Clone</button>
-    </div>
+    <transition enter-active-class="animate__animated animate__slideInRight">
+      <div class="flex flex-row items-start" v-show="formVisible">
+        <validation-observer ref="form">
+          <form @submit.prevent="submitForm" class="form-horizontal form-horizontal-slim">
+            <v-select-field rules="required" field_class="field-tall" v-model="baseField" field_label="Base Field" field_id="baseFieldSelectToClone" :options="baseFields"></v-select-field>
+          </form>
+        </validation-observer>
+        <button @click="submitForm()" class="btn btn-green ml-2"><font-awesome-icon icon="clone"></font-awesome-icon> Clone</button>
+      </div>
+    </transition>
   </div>
 </template>
 
