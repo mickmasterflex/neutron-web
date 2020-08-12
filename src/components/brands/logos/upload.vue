7f3f88196767f24bccb3ebe9639c4248e1c10a87
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-12 12:03:27 -0600

    adding validation for image field, importing additional icons (check-circle, image)

--- src/components/brands/logos/upload.vue
+++ src/components/brands/logos/upload.vue
@@ -4,11 +4,14 @@
     <validation-observer v-slot="{ handleSubmit }" ref="form">
       <form @submit.prevent="handleSubmit(uploadFile)">
         <div>
-          <input type="file" @change="onSelectedFile" ref="fileInput" style="display: none">
+          <validation-provider rules="image" vid="image" name="image" v-slot="{ errors }" ref="provider">
+            <input type="file" @change="onSelectedFile" ref="fileInput" style="display: none"/>
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
           <span class="btn btn-turquoise" @click="$refs.fileInput.click()">
-            <font-awesome-icon icon="plus"></font-awesome-icon> Select a File
+            <font-awesome-icon icon="image"></font-awesome-icon> Select a File
           </span>
-          <span v-if="selectedFile">{{selectedFile.name}}</span>
+          <span v-if="selectedFile" class="text-gray-900"><font-awesome-icon icon="check-circle" class="text-green-600"></font-awesome-icon> {{selectedFile.name}}</span>
         </div>
         <text-field v-model="alt" field_id="alt" field_label="Alt Text"></text-field>
         <button class="btn btn-green" :disabled="!selectedFile">Upload</button>
@@ -19,6 +22,7 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { setResponseErrors } from '@/mixins/setResponseErrors'
 
 export default {
   data () {
@@ -27,6 +31,7 @@ export default {
       alt: ''
     }
   },
+  mixins: [setResponseErrors],
   computed: {
     ...mapGetters({
       brand: 'getCurrentBrand'
@@ -36,8 +41,12 @@ export default {
     ...mapActions({
       uploadBrandLogo: 'uploadBrandLogo'
     }),
-    onSelectedFile (e) {
-      this.selectedFile = e.target.files[0]
+    async onSelectedFile (e) {
+      const { valid } = await this.$refs.provider.validate(e)
+
+      if (valid) {
+        this.selectedFile = e.target.files[0]
+      }
     },
     uploadFile () {
       this.$refs.form.validate().then(success => {
@@ -46,8 +55,8 @@ export default {
           formData.append('image', this.selectedFile)
           formData.append('brand', this.brand.id)
           formData.append('alt', this.alt)
-          this.uploadBrandLogo(formData).catch(e => {
-            window.console.log(e.response)
+          this.uploadBrandLogo(formData).catch(error => {
+            this.error = error
           })
         }
       })
