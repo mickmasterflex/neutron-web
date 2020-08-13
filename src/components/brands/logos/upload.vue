fec62422c62aff133f4c84eee0907b32932ac981
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-13 17:39:27 -0600

    method for setting image to selected file. updating data name

--- src/components/brands/logos/upload.vue
+++ src/components/brands/logos/upload.vue
@@ -2,9 +2,9 @@
   <div class="well well-light">
     <validation-observer v-slot="{ handleSubmit }" ref="form">
       <form @submit.prevent="handleSubmit(uploadFile)" class="form-horizontal">
-        <file-field @input="selectedFile = $event" rules="image" field_id="image" field_label="Upload a Logo"></file-field>
+        <file-field @input="fileSelected($event)" rules="image" field_id="image" field_label="Upload a Logo"></file-field>
         <text-field v-model="alt" field_id="alt" field_label="Alt Text"></text-field>
-        <button class="btn btn-green" :disabled="!selectedFile">Upload</button>
+        <button class="btn btn-green" :disabled="!image">Upload</button>
       </form>
     </validation-observer>
   </div>
@@ -18,7 +18,7 @@ import fileField from '@/components/ui/forms/validation-fields/file-field'
 export default {
   data () {
     return {
-      selectedFile: null,
+      image: null,
       alt: ''
     }
   },
@@ -37,11 +37,14 @@ export default {
     ...mapActions({
       uploadBrandLogo: 'uploadBrandLogo'
     }),
+    fileSelected (file) {
+      this.image = file
+    },
     uploadFile () {
       this.$refs.form.validate().then(success => {
         if (success) {
           const formData = new FormData()
-          formData.append('image', this.selectedFile)
+          formData.append('image', this.image)
           formData.append('brand', this.brand.id)
           formData.append('alt', this.alt)
           this.uploadBrandLogo(formData).catch(error => {
