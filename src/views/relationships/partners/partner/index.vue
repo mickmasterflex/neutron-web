445f5f4f28d538df62324a7b27865200eb70aaec
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-14 15:55:12 -0600

    delete partner contract to use client slug

--- src/views/relationships/partners/partner/index.vue
+++ src/views/relationships/partners/partner/index.vue
@@ -4,7 +4,7 @@
       <h3 class="h3 mb-2">Update Partner Contract</h3>
       <update-partner-contract :partner="partner"></update-partner-contract>
       <h3 class="h3 mt-5 mb-2">Delete Partner Contract</h3>
-      <delete-partner-contract :client="partner.client" :id="id"></delete-partner-contract>
+      <delete-partner-contract :client="$route.params.client" :id="id"></delete-partner-contract>
     </template>
   </partner-layout>
 </template>
