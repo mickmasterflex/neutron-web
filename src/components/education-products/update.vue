1149bf22c4e10409f88f0ad781b201a78d9967c3
Kevin Bigelow <kbigelow5@gmail.com>
2020-08-18 11:21:05 -0600

    implementing panel on education product update

--- src/components/education-products/update.vue
+++ src/components/education-products/update.vue
@@ -1,14 +1,20 @@
 <template>
-  <validation-observer v-slot="{ handleSubmit }" ref="form">
-    <form @submit.prevent="handleSubmit(submitForm)">
-      <v-text-field v-model="name" rules="required" field_id="Name" field_label="Education Product Name"></v-text-field>
-      <textarea-field v-model="short_description" field_id="short_description" field_label="Short Description"></textarea-field>
-      <textarea-field v-model="description" field_id="description" field_label="Description"></textarea-field>
-      <select-field v-model="area_of_interest" :options="interestOptions" field_id="area_of_interest" field_label="Area Of Interest"></select-field>
-      <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degree_level" field_label="Degree Level"></select-field>
-      <button type="submit" class="btn btn-green mt-5">Submit</button>
-    </form>
-  </validation-observer>
+  <form-panel title="Edit Education Product" :actionTransition="true">
+    <template v-slot:action>
+      <button @click="submitForm" class="btn btn-green" v-show="unsavedChanges">Save Changes</button>
+    </template>
+    <template v-slot:content>
+      <validation-observer ref="form" class="form-horizontal">
+        <form @submit.prevent="submitForm">
+          <v-text-field v-model="name" rules="required" field_id="Name" field_label="Name"></v-text-field>
+          <textarea-field v-model="short_description" field_id="short_description" field_label="Short Description"></textarea-field>
+          <textarea-field v-model="description" field_id="description" field_label="Description"></textarea-field>
+          <select-field v-model="area_of_interest" :options="interestOptions" field_id="area_of_interest" field_label="Area Of Interest"></select-field>
+          <select-field v-model="degree_level" :options="degreeLevelOptions" field_id="degree_level" field_label="Degree Level"></select-field>
+        </form>
+      </validation-observer>
+    </template>
+  </form-panel>
 </template>
 
 <script>
@@ -64,21 +70,34 @@ export default {
       this.product_group = this.educationProduct.product_group
     }
   },
+  computed: {
+    unsavedChanges () {
+      if (this.name) {
+        return this.name !== this.educationProduct.name || this.short_description !== this.educationProduct.short_description || this.description !== this.educationProduct.description || this.area_of_interest !== this.educationProduct.area_of_interest || this.degree_level !== this.educationProduct.degree_level || this.product_group !== this.educationProduct.product_group
+      } else {
+        return false
+      }
+    }
+  },
   methods: {
     ...mapActions({
       update: 'updateEducationProduct'
     }),
     submitForm () {
-      this.update({
-        name: this.name,
-        short_description: this.short_description,
-        description: this.description,
-        area_of_interest: this.area_of_interest,
-        degree_level: this.degree_level,
-        product_group: this.product_group,
-        id: this.educationProduct.id
-      }).catch(error => {
-        this.error = error
+      this.$refs.form.validate().then(success => {
+        if (success) {
+          this.update({
+            name: this.name,
+            short_description: this.short_description,
+            description: this.description,
+            area_of_interest: this.area_of_interest,
+            degree_level: this.degree_level,
+            product_group: this.product_group,
+            id: this.educationProduct.id
+          }).catch(error => {
+            this.error = error
+          })
+        }
       })
     }
   }
