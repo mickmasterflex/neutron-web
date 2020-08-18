c127185ba3290998fab01c5a87ec85bf29c1515a
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-18 14:24:10 -0600

    replacing slug with .params.slug

--- src/components/clients/update.vue
+++ src/components/clients/update.vue
@@ -43,7 +43,7 @@ export default {
       })
     }
   },
-  create () {
+  created () {
     this.setClient()
   }
 }
--- src/views/relationships/clients/client/contracts.vue
+++ src/views/relationships/clients/client/contracts.vue
@@ -8,7 +8,7 @@
         <button class="btn btn-turquoise" @click="showCreateBuyerModal()"><font-awesome-icon icon="plus"></font-awesome-icon> Add Buyer</button>
       </template>
     </action-heading>
-    <buyer-list :contracts="buyers" :client="slug"></buyer-list>
+    <buyer-list :contracts="buyers" :client="$route.params.slug"></buyer-list>
     <create-buyer-contract :client="client.id" :buyer-contracts="buyers"></create-buyer-contract>
 
     <action-heading class="mt-5">
@@ -19,7 +19,7 @@
         <button class="btn btn-turquoise" @click="showCreatePartnerModal()"><font-awesome-icon icon="plus"></font-awesome-icon> Add Partner</button>
       </template>
     </action-heading>
-    <partner-list :contracts="partners" :client="slug"></partner-list>
+    <partner-list :contracts="partners" :client="$route.params.slug"></partner-list>
     <create-partner-contract :client="client.id" :partner-contracts="partners"></create-partner-contract>
   </div>
   </template>
