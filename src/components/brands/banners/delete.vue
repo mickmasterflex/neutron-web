4b643c857ed65c075afa111dbc3f1a482667e4e6
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-24 11:18:23 -0600

    banner upload/delete/banner.js

---
+++ src/components/brands/banners/delete.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn btn-red" @click="runDelete()"><font-awesome-icon icon="trash-alt"></font-awesome-icon> Delete Banner</button>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions({
+      deleteBrandBanner: 'deleteBrandBanner'
+    }),
+    runDelete () {
+      this.brandBanners.forEach(banner => {
+        if (banner.brand === this.brand.id) {
+          this.deleteBrandBanner(banner.id)
+        }
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      brandBanners: 'getCurrentBrandBanner',
+      brand: 'getCurrentBrand'
+    })
+  }
+}
+</script>
---
+++ src/store/modules/brands/banners.js
@@ -0,0 +1,43 @@
+import axios from '@/axios'
+
+const state = {
+  current_brand_banners: []
+}
+
+const getters = {
+  getCurrentBrandBanners: state => state.current_brand_banners
+}
+
+const actions = {
+  async fetchCurrentBrandBanners ({ commit }, brand) {
+    await axios.get(`/brand-banners/?brand=${brand}/`)
+      .then(response => {
+        commit('SET_CURRENT_BRAND_BANNERS', response.data)
+      })
+  },
+  async uploadBrandBanner ({ commit }, banner) {
+    await axios.post('/brand-banners/', banner)
+      .then(response => {
+        commit('ADD_BANNERS', response.data)
+      })
+  },
+  async deleteBrandBanner ({ commit }, id) {
+    await axios.delete(`/brand-banners/${id}/`)
+      .then(() => {
+        commit('REMOVE_BANNER', id)
+      })
+  }
+}
+
+const mutations = {
+  SET_CURRENT_BRAND_BANNERS: (state, banners) => (state.current_brand_banners = banners),
+  ADD_BANNERS: (state, banners) => (state.current_brand_banners.unshift(banners)),
+  REMOVE_BANNER: (state, id) => (state.current_brand_banners = state.current_brand_banners.filter(banner => banner.id !== id))
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
