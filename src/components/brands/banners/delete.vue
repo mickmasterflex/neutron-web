08a0cb9fab3fd5d15349ea7587caec696898ad38
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-24 12:07:28 -0600

    correcting delete.vue minor change

--- src/components/brands/banners/delete.vue
+++ src/components/brands/banners/delete.vue
@@ -20,7 +20,7 @@ export default {
   },
   computed: {
     ...mapGetters({
-      brandBanners: 'getCurrentBrandBanner',
+      brandBanners: 'getCurrentBrandBanners',
       brand: 'getCurrentBrand'
     })
   }
