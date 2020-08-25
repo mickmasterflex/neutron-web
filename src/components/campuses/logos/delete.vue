58e29c3548ee9600dcb1f4c604cff40d00815423
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-25 13:38:43 -0600

    Campus Logos upload, delete and list

---
+++ src/components/campuses/logos/delete.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn btn-red" @click="runDelete()"><font-awesome-icon icon="trash-alt"></font-awesome-icon> Delete Logo</button>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions({
+      deleteCampusLogo: 'deleteCampusLogo'
+    }),
+    runDelete () {
+      this.campusLogos.forEach(logo => {
+        if (logo.campus === this.campus.id) {
+          this.deleteCampusLogo(logo.id)
+        }
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      campusLogos: 'getCurrentCampusLogos',
+      campus: 'getCurrentCampus'
+    })
+  }
+}
+</script>
--- src/views/products/campus/details.vue
+++ src/views/products/campus/details.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
     <update-campus :campus="campus"></update-campus>
+
+    <campus-logos v-show="campusLogos.length"></campus-logos>
+    <upload-logo v-show="!campusLogos.length"></upload-logo>
+
     <delete-campus :campus="campus" class="mt-4"></delete-campus>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import deleteCampus from '@/components/campuses/delete'
 import updateCampus from '@/components/campuses/update'
+import campusLogos from '@/components/campuses/logos/list'
+import uploadLogo from '@/components/campuses/logos/upload'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   components: {
     'delete-campus': deleteCampus,
-    'update-campus': updateCampus
+    'update-campus': updateCampus,
+    'campus-logos': campusLogos,
+    'upload-logo': uploadLogo
   },
   computed: {
     ...mapGetters({
-      campus: 'getCurrentCampus'
+      campus: 'getCurrentCampus',
+      campusLogos: 'getCurrentCampusLogos'
+    })
+  },
+  created () {
+    this.fetchLogos(this.campus.id)
+  },
+  methods: {
+    ...mapActions({
+      fetchLogos: 'fetchCurrentCampusLogos'
     })
   }
 }
