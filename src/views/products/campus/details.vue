f6956e2887ab430aaaf9815fe720a83e3099c0a2
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-08-27 11:24:47 -0600

    adding upload and campusBanners to details.vue/campus

--- src/views/products/campus/details.vue
+++ src/views/products/campus/details.vue
@@ -5,6 +5,9 @@
     <campus-logos v-show="campusLogos.length"></campus-logos>
     <upload-logo v-show="!campusLogos.length"></upload-logo>
 
+    <campus-banners v-show="campusBanners.length"></campus-banners>
+    <upload-banner v-show="!campusBanners.length"></upload-banner>
+
     <delete-campus :campus="campus" class="mt-4"></delete-campus>
   </div>
 </template>
@@ -14,31 +17,39 @@ import deleteCampus from '@/components/campuses/delete'
 import updateCampus from '@/components/campuses/update'
 import campusLogos from '@/components/campuses/logos/list'
 import uploadLogo from '@/components/campuses/logos/upload'
+import campusBanners from '@/components/campuses/banners/list'
+import uploadBanner from '@/components/campuses/banners/upload'
+
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
   computed: {
     ...mapGetters({
       campus: 'getCurrentCampus',
-      campusLogos: 'getCurrentCampusLogos'
+      campusLogos: 'getCurrentCampusLogos',
+      campusBanners: 'getCurrentCampusBanners'
     })
   },
   components: {
     'delete-campus': deleteCampus,
     'update-campus': updateCampus,
     'campus-logos': campusLogos,
-    'upload-logo': uploadLogo
+    'upload-logo': uploadLogo,
+    'campus-banners': campusBanners,
+    'upload-banner': uploadBanner
   },
   watch: {
     campus () {
       if (this.campus) {
         this.fetchLogos(this.campus.id)
+        this.fetchBanners(this.campus.id)
       }
     }
   },
   methods: {
     ...mapActions({
-      fetchLogos: 'fetchCurrentCampusLogos'
+      fetchLogos: 'fetchCurrentCampusLogos',
+      fetchBanners: 'fetchCurrentCampusBanners'
     })
   }
 }
