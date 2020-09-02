9af53f761037054d491a2b34488b1e2cae047be8
Kevin Bigelow <kbigelow5@gmail.com>
2020-09-02 12:26:49 -0600

    some simple conditions based on selected_day

--- src/components/caps/day/index.vue
+++ src/components/caps/day/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <div v-if="selected_day.attributes">{{selected_day.attributes[0].customData.date}} {{selected_day.attributes[0].customData.limit}}</div>
-    <v-calendar ref="calendar" :attributes="attributes" @dayclick="dayClicked">
+    <div v-if="selected_day">
+      {{selected_day.date}}
+      <span v-if="selected_day.attributes">
+        {{selected_day.attributes[0].customData.limit}}
+      </span>
+    </div>
+    <v-calendar ref="calendar" :attributes="attributes" @dayclick="dayClicked" :class="[selected_day.date ? 'opacity-25' : '']">
       <template v-slot:day-content="slotProps">
-        <div class="border-2 border-gray-200">
+        <div class="border-2 border-gray-200 cursor-pointer" @click="dayClicked(slotProps.day)">
           {{ slotProps.day.day }}
-          <span v-if="slotProps.day.attributes" class="text-blue-500" @click="dayClicked(slotProps.day)">
+          <span v-if="slotProps.day.attributes" class="text-blue-500">
             {{slotProps.day.attributes[0].customData.limit}}
           </span>
-          <span v-else class="text-blue-500" @click="dayClicked(slotProps.day)">
+          <span v-else class="text-blue-500">
             +
           </span>
         </div>
@@ -31,7 +36,6 @@ export default {
   methods: {
     dayClicked (day) {
       this.selected_day = day
-      window.console.log(day)
     }
   },
   computed: {
