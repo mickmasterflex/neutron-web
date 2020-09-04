1f20c125e3bb16fd876ce1449a818c0d45a0abe8
Kevin Bigelow <kbigelow5@gmail.com>
2020-09-04 11:54:54 -0600

    full-calendar title styling

--- src/views/relationships/buyers/buyer/details.vue
+++ src/views/relationships/buyers/buyer/details.vue
@@ -5,7 +5,7 @@
       <template v-slot:content>
         <day-cap-create></day-cap-create>
         <day-cap-update></day-cap-update>
-        <day-caps class="mt-3"></day-caps>
+        <day-caps class="my-3"></day-caps>
       </template>
     </panel-template>
     <panel-template title="Danger Zone">
