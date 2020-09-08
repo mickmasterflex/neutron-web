54be5e0374632458373e8314d39500dacda603c6
Kevin Bigelow <kbigelow5@gmail.com>
2020-09-08 16:20:57 -0600

    adding modalDialogClass prop to base modal, adding top-30 class, using class for panel-modal

--- src/components/ui/modals/panel-modal.vue
+++ src/components/ui/modals/panel-modal.vue
@@ -2,6 +2,7 @@
   <modal-template
     :allowScroll="true"
     modalClass="modal-xs"
+    modalDialogClass="top-30"
     modalPosition="absolute"
     modalBackdropColor="bg-gray-100"
     class="rounded-b-xl"
