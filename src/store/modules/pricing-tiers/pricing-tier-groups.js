4b05fa0015fb0818df767686c4769eff16ddcc79
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-09-09 09:43:23 -0600

    adding pricing-tiers files

---
+++ src/store/modules/pricing-tiers/pricing-tier-groups.js
@@ -0,0 +1,51 @@
+import axios from '@/axios'
+
+const state = {
+  pricing_tier_groups: [],
+  current_pricing_tier_group: {},
+  show_update_pricing_tier_group_modal: false
+}
+
+const getters = {
+  getPricingTierGroups: state => state.pricing_tier_groups,
+  getCurrentPricingTierGroup: state => state.current_pricing_tier_group,
+  getShowUpdatePricingTierGroupModal: state => state.show_update_pricing_tier_group_modal
+}
+
+const actions = {
+  async fetchPricingTierGroups () {
+    await axios.get('/pricing-tier-groups/')
+  },
+  async createPricingTierGroup (data) {
+    await axios.get('/pricing-tier-groups/', data)
+  },
+  async updatePricingTierGroup (data) {
+    await axios.get(`/pricing-tier-groups/${data.id}/`, data)
+  },
+  async deletePricingTierGroup (id) {
+    await axios.get(`/pricing-tier-groups/${id}/`)
+  }
+}
+
+const mutations = {
+  SHOW_UPDATE_PRICING_TIER_GROUP_MODAL: (state) => (state.show_update_pricing_tier_group_modal = true),
+  CLOSE_UPDATE_PRICING_TIER_GROUP_MODAL: (state) => (state.show_update_pricing_tier_group_modal = false),
+  SET_PRICING_TIER_GROUPS: (state, groups) => (state.pricing_tier_groups = groups),
+  SET_CURRENT_PRICING_TIER_GROUP: (state, group) => (state.current_pricing_tier_group = group),
+  RESET_CURRENT_PRICING_TIER_GROUP: (state) => (state.current_pricing_tier_group = {}),
+  ADD_PRICING_TIER_GROUP: (state, group) => state.pricing_tier_groups.unshift(group),
+  REMOVE_PRICING_TIER_GROUP: (state, id) => (state.pricing_tier_groups = state.pricing_tier_groups.filter(group => group.id !== id)),
+  UPDATE_PRICING_TIER_GROUP: (state, updatedGroup) => {
+    const index = state.pricing_tier_groups.findIndex(group => group.id === updatedGroup.id)
+    if (index !== -1) {
+      state.pricing_tier_groups.splice(index, 1, updatedGroup)
+    }
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
