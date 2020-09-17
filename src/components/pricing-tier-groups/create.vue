282d9967056d0778cf0f040466daba3b7b3a045a
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-09-17 14:01:44 -0600

    small changes/fixes to syntax

--- src/components/pricing-tier-groups/create.vue
+++ src/components/pricing-tier-groups/create.vue
@@ -5,7 +5,7 @@
       <div class="flex flex-row items-start" v-show="formVisible">
         <validation-observer ref="form">
           <form @submit.prevent="submitForm" class="form-horizontal form-horizontal-slim">
-            <v-text-field v-model="name" rules="required" field_id="name" field_label="Group Name"></v-text-field>
+            <v-text-field field_class="field-tall" v-model="name" rules="required" field_id="name" field_label=" Name"></v-text-field>
           </form>
         </validation-observer>
         <button @click="submitForm()" class="btn btn-green ml-2"><font-awesome-icon icon="plus"></font-awesome-icon> Create Group</button>
--- src/components/pricing-tier-groups/list.vue
+++ src/components/pricing-tier-groups/list.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-  <panel-template v-for="pricingTierGroup in pricingTierGroups" :title="pricingTierGroup.name" :key="pricingTierGroup.id">
-   <template v-slot:action>
-     <span class="flex flex-row justify-end">
-              <delete-pricing-tier-group :id="pricingTierGroup.id" :type="pricingTierGroup.type"></delete-pricing-tier-group>
-            <button @click="showModal(pricingTierGroup)" class="btn btn-blue btn-circle"><font-awesome-icon icon="pencil-alt"></font-awesome-icon></button>
-    <update-pricing-tier-group></update-pricing-tier-group>
-     </span>
-  </template>
-  <template v-slot:content>
-    LIST TIERS HERE
-  </template>
-  </panel-template>
+    <panel-template v-for="pricingTierGroup in pricingTierGroups" :title="pricingTierGroup.name" :key="pricingTierGroup.id">
+      <template v-slot:action>
+        <span class="flex flex-row justify-end">
+          <delete-pricing-tier-group :id="pricingTierGroup.id" :type="pricingTierGroup.type"></delete-pricing-tier-group>
+          <button @click="showModal(pricingTierGroup)" class="btn btn-hollow-blue btn-circle"><font-awesome-icon icon="pencil-alt"></font-awesome-icon></button>
+          <update-pricing-tier-group></update-pricing-tier-group>
+        </span>
+       </template>
+      <template v-slot:content>
+        LIST TIERS HERE
+      </template>
+    </panel-template>
   </div>
 </template>
 
