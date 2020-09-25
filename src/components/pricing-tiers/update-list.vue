6c6ef52fb53fb9588647b978295459adf2eb617c
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-09-25 11:36:17 -0600

    update-list for pricing tiers

---
+++ src/components/pricing-tiers/update-list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table v-if="pricingTiers" class="table">
+      <thead>
+      <tr>
+        <th class="th">Lower Bound</th>
+        <th class="th">Upper Bound</th>
+        <th class="th">Payout</th>
+      </tr>
+      </thead>
+      <tbody class="tbody">
+      <tr class="tr" v-for="pricingTier in pricingTiers" :key="pricingTier.id">
+        <td class="td">{{pricingTier.lower_bound}}</td>
+        <td class="td">{{pricingTier.upper_bound}}</td>
+        <td class="td">{{pricingTier.payout}}</td>
+      </tr>
+      </tbody>
+    </table>
+    <table-empty-state v-else heading="No Tiers Added" copy="Edit the pricing tier group to add tiers"></table-empty-state>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      pricingTiers: 'getCurrentPricingTiers'
+    })
+  }
+}
+</script>
---
+++ src/store/modules/pricing-tiers/pricing-tiers.js
@@ -0,0 +1,59 @@
+import axios from '@/axios'
+
+const state = {
+  current_pricing_tiers: {},
+  show_update_pricing_tier_modal: false
+}
+
+const getters = {
+  getCurrentPricingTiers: state => state.current_pricing_tiers,
+  getShowUpdatePricingTierModal: state => state.show_update_pricing_tier_modal
+}
+const actions = {
+  async fetchPricingTiers ({ commit }) {
+    await axios.get('/pricing-tiers/')
+      .then(response => {
+        commit('SET_PRICING_TIERS', response.data)
+      })
+  },
+  async createPricingTier ({ commit }, data) {
+    await axios.post('/pricing-tiers/', data)
+      .then(response => {
+        commit('ADD_PRICING_TIER', response.data)
+      })
+  },
+  async updatePricingTier ({ commit }, data) {
+    await axios.put(`/pricing-tiers/${data.id}/`, data)
+      .then(response => {
+        commit('UPDATE_PRICING_TIER', response.data)
+      })
+  },
+  async deletePricingTier ({ commit }, id) {
+    await axios.delete(`/pricing-tiers/${id}/`)
+      .then(() => {
+        commit('REMOVE_PRICING_TIER', id)
+      })
+  }
+}
+
+const mutations = {
+  SHOW_UPDATE_PRICING_TIER_MODAL: (state) => (state.show_update_pricing_tier_modal = true),
+  CLOSE_UPDATE_PRICING_TIER_MODAL: (state) => (state.show_update_pricing_tier_modal = false),
+  SET_CURRENT_PRICING_TIERS: (state, tier) => (state.current_pricing_tiers = tier),
+  RESET_CURRENT_PRICING_TIERS: (state) => (state.current_pricing_tiers = {}),
+  ADD_PRICING_TIER: (state, tier) => state.pricing_tiers.unshift(tier),
+  REMOVE_PRICING_TIER: (state, id) => (state.pricing_tiers = state.pricing_tiers.filter(tier => tier.id !== id)),
+  UPDATE_PRICING_TIER: (state, updatedTier) => {
+    const index = state.pricing_tiers.findIndex(tier => tier.id === updatedTier.id)
+    if (index !== -1) {
+      state.pricing_tiers.splice(index, 1, updatedTier)
+    }
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
