f3452c77e8176c57675d7e31306545acaf1ea3e3
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-09-29 11:57:42 -0600

    fixing file name

--- src/store/modules/pricing-tiers/index.js
+++ src/store/modules/pricing-tiers/index.js
@@ -1,5 +1,5 @@
 import pricingTierGroups from '@/store/modules/pricing-tiers/pricing-tier-groups'
-import pricingTiers from '@/store/modules/pricing-tiers/pricing-tier'
+import pricingTiers from '@/store/modules/pricing-tiers/pricing-tiers'
 
 const modules = {
   pricingTierGroups,
