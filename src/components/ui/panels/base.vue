3bc07e94afc0732a31f24ee349a9cbfba253da77
Kevin Bigelow <kbigelow5@gmail.com>
2020-09-30 16:30:27 -0600

    adding loader to panel component

--- src/components/ui/panels/base.vue
+++ src/components/ui/panels/base.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="panel rounded-xl bg-gray-100 border-gray-200 border-2">
+  <div class="panel rounded-xl bg-gray-100 border-gray-200 border-2 relative">
+    <div v-show="showLoader" class="absolute inset-0 bg-white opacity-75 flex flex-row items-center justify-center">
+      <font-awesome-icon class="text-gray-900 text-lg" icon="spinner" pulse></font-awesome-icon>
+      <p class="h4 ml-1 mb-0 pb-0 font-bold">{{ loadingText }}</p>
+    </div>
     <div class="flex flex-row justify-between items-center bg-white rounded-t-xl p-3 pl-4" style="height: 70px;">
       <h3 class="h4 m-0">{{title}}</h3>
       <transition v-if="actionTransition" enter-active-class="animate__animated animate__bounceIn" leave-active-class="animate__animated animate__fast animate__fadeOut">
@@ -26,7 +30,15 @@ export default {
       type: Boolean,
       default: false
     },
-    contentClass: String
+    contentClass: String,
+    loadingText: {
+      default: 'Loading',
+      type: String
+    },
+    showLoader: {
+      default: false,
+      type: Boolean
+    }
   }
 }
 </script>
