7c4b5900019198b62adefa6861d292193355c6bb
Kevin Bigelow <kbigelow5@gmail.com>
2020-09-30 11:18:12 -0600

    modifying tier conditions

--- src/components/pricing-tiers/update-list.vue
+++ src/components/pricing-tiers/update-list.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="well">
-    <div v-if="pricingTiers">
     <ul class="fields-inline-heading px-1">
-      <li class="w-64 fields-inline-heading-item">Lower Bound</li>
-      <li class="w-64 fields-inline-heading-item">Upper Bound</li>
-      <li class="w-64 fields-inline-heading-item">Payout</li>
+      <li class="w-40 fields-inline-heading-item">Lower Bound</li>
+      <li class="w-40 fields-inline-heading-item">Upper Bound</li>
+      <li class="w-40 fields-inline-heading-item">Payout</li>
     </ul>
     <ul>
       <li class="card card-sm mb-1 flex flex-row items-center justify-between" v-for="pricingTier in pricingTiers" :key="pricingTier.id">
@@ -14,8 +13,6 @@
         <delete-pricing-tier :id="pricingTiers.id"></delete-pricing-tier>
       </li>
     </ul>
-  </div>
-    <table-empty-state v-else heading="No Tiers Added" copy="Edit the pricing tier group to add tiers"></table-empty-state>
   </div>
 </template>
 
