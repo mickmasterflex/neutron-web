489f7678d9de579ee730c0dac8353daa666c47f0
Kevin Bigelow <kbigelow5@gmail.com>
2020-10-13 12:56:57 -0600

    updating placeholder for location field

--- src/components/geos/locations-field.vue
+++ src/components/geos/locations-field.vue
@@ -5,13 +5,14 @@
     field_label="Locations"
     rules="required"
     v-model="inner_value"
-    placeholder="Enter multiple postal codes, or abbreviated states. Separate by return or comma. ie: 84104, UT, 90210, UT, TX"></v-textarea-field>
+    :placeholder="placeholder"></v-textarea-field>
 </template>
 
 <script>
 export default {
   data: () => ({
-    inner_value: ''
+    inner_value: '',
+    placeholder: 'Enter multiple postal codes or abbreviated states.\nSeparate with commas, ie:\n84104, UT, 90210, ny, TX'
   }),
   watch: {
     // Handles internal model changes.
