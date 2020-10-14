6fec78693f5c4afdda5b577cbde96674059cb011
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-10-14 10:58:49 -0600

    Pricing Tier Group added to Partner Create

--- src/components/partners/create.vue
+++ src/components/partners/create.vue
@@ -5,6 +5,8 @@
   <validation-observer ref="form">
     <form @submit.prevent="submitForm" class="form-horizontal">
       <v-text-field v-model="name" rules="required" field_id="name" field_label="Name"></v-text-field>
+      <v-select-field v-model="pricing_tier_group" :options="pricingTierGroups" field_label="Pricing Tier Group"></v-select-field>
+      <list-tiers class="ml-label-width" tableWidth="auto" emptyTableClass="max-w-sm well" v-if="currentGroup" :pricingTiers='currentGroup.pricingtier_set'></list-tiers>
     </form>
   </validation-observer>
     </template>
@@ -15,6 +17,7 @@
 </template>
 
 <script>
+import listTiers from '@/components/pricing-tiers/list'
 import { mapActions, mapGetters, mapMutations } from 'vuex'
 import { enterKeyListener } from '@/mixins/enter-key-listener'
 import { setResponseErrors } from '@/mixins/set-response-errors'
@@ -22,13 +25,22 @@ import { setResponseErrors } from '@/mixins/set-response-errors'
 export default {
   data () {
     return {
-      name: ''
+      name: '',
+      pricing_tier_group: ''
     }
+  },
+  components: {
+    'list-tiers': listTiers
   },
   computed: {
     ...mapGetters({
-      showModal: 'getShowCreatePartnerModal'
-    })
+      showModal: 'getShowCreatePartnerModal',
+      pricingTierGroups: 'getPricingTierGroups',
+      getPricingTierGroupById: 'getPricingTierGroupById'
+    }),
+    currentGroup () {
+      return this.getPricingTierGroupById(Number(this.pricing_tier_group))
+    }
   },
   props: {
     client: {
@@ -41,7 +53,8 @@ export default {
   mixins: [enterKeyListener, setResponseErrors],
   methods: {
     ...mapActions({
-      create: 'createPartner'
+      create: 'createPartner',
+      fetchPricingTierGroupsGroups: 'fetchPricingTierGroups'
     }),
     ...mapMutations({
       closeModal: 'CLOSE_CREATE_PARTNER_MODAL'
@@ -59,7 +72,8 @@ export default {
           this.create({
             name: this.name,
             parent: this.parent,
-            client: this.$props.client
+            client: this.$props.client,
+            pricing_tier_group: this.pricing_tier_group
           }).then(() => {
             this.close()
           }).catch(error => {
