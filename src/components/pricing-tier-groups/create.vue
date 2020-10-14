6f848c2aaf5e475f96a462158fb0f21a86db31af
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-10-14 16:34:39 -0600

    focus on name field for pricing tier groups

--- src/components/pricing-tier-groups/create.vue
+++ src/components/pricing-tier-groups/create.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <button class="btn btn-turquoise" @click="showForm()" v-show="!formVisible"><font-awesome-icon icon="plus"></font-awesome-icon> Add Pricing Tier Group</button>
-    <transition enter-active-class="animate__animated animate__slideInRight animate__fast">
+    <transition enter-active-class="animate__animated animate__slideInRight animate__fast" v-on:after-enter="$refs.focusField.focusOnField()">
       <div class="flex flex-row items-start" v-show="formVisible">
         <validation-observer ref="form">
           <form @submit.prevent="submitForm" class="form-horizontal form-horizontal-slim">
-            <v-text-field field_class="field-tall" v-model="name" rules="required" field_id="name" field_label=" Name"></v-text-field>
+            <v-text-field ref="focusField" field_class="field-tall" v-model="name" rules="required" field_id="name" field_label=" Name"></v-text-field>
           </form>
         </validation-observer>
         <button @click="submitForm()" class="btn btn-green ml-2"><font-awesome-icon icon="plus"></font-awesome-icon> Create Group</button>
