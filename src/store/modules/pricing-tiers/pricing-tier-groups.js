79111cb55d1d70bb13d49a31c167b286df93f008
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-10-16 14:39:34 -0600

    alphabetizing pricing tier groups

--- src/store/modules/pricing-tiers/pricing-tier-groups.js
+++ src/store/modules/pricing-tiers/pricing-tier-groups.js
@@ -17,12 +17,14 @@ const actions = {
     await axios.get('/pricing-tier-groups/')
       .then(response => {
         commit('SET_PRICING_TIER_GROUPS', response.data)
+        commit('SORT_PRICING_TIER_GROUPS', response.data)
       })
   },
   async createPricingTierGroup ({ commit }, data) {
     await axios.post('/pricing-tier-groups/', data)
       .then(response => {
         commit('ADD_PRICING_TIER_GROUP', response.data)
+        commit('SORT_PRICING_TIER_GROUPS', response.data)
       })
   },
   async updatePricingTierGroup ({ commit }, data) {
@@ -52,9 +54,22 @@ const mutations = {
     if (index !== -1) {
       state.pricing_tier_groups.splice(index, 1, updatedGroup)
     }
+  },
+  SORT_PRICING_TIER_GROUPS: (state) => {
+    function compare (a, b) {
+      const groupA = a.name.toUpperCase()
+      const groupB = b.name.toUpperCase()
+      let comparison = 0
+      if (groupA > groupB) {
+        comparison = 1
+      } else if (groupA < groupB) {
+        comparison = -1
+      }
+      return comparison
+    }
+    state.pricing_tier_groups.sort(compare)
   }
 }
-
 export default {
   state,
   getters,
