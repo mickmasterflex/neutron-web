94c3c5c6f1fb669abc612afecebb7913fa4f77c0
zackckpetersen <zackcpetersen@gmail.com>
2020-10-20 16:24:15 -0600

    fixing logo alt text bug

--- src/components/brands/logos/upload.vue
+++ src/components/brands/logos/upload.vue
@@ -7,7 +7,7 @@
       <validation-observer ref="form">
         <form @submit.prevent="uploadFile()" class="form-horizontal">
           <file-field @input="fileSelected($event)" rules="image" field_id="image" field_label="Logo"></file-field>
-          <text-field v-model="alt" field_id="alt" field_label="Alt Text"></text-field>
+          <text-field v-model="alt" :value="altBrand" field_id="alt" field_label="Alt Text"></text-field>
         </form>
       </validation-observer>
     </template>
@@ -30,17 +30,20 @@ export default {
   computed: {
     ...mapGetters({
       brand: 'getCurrentBrand'
-    })
-  },
-  watch: {
-    brand () {
-      this.alt = this.brand.name + ' logo'
+    }),
+    altBrand: function () {
+      const defaultAlt = this.brand.name + ' logo'
+      this.setAlt(defaultAlt)
+      return defaultAlt
     }
   },
   methods: {
     ...mapActions({
       uploadBrandLogo: 'uploadBrandLogo'
     }),
+    setAlt (alt) {
+      this.alt = alt
+    },
     fileSelected (file) {
       this.image = file
     },
