7a5a65a21fdeb3938bf1aa4c48e1b683742b3e40
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-10-20 10:22:05 -0600

    sorting pricing tiers list

--- src/store/modules/pricing-tiers/tiers.js
+++ src/store/modules/pricing-tiers/tiers.js
@@ -12,6 +12,7 @@ const actions = {
     await axios.post('/pricing-tiers/', data)
       .then(response => {
         commit('ADD_PRICING_TIER', response.data)
+        commit('SORT_PRICING_TIERS', response.data)
       })
   },
   async deletePricingTier ({ commit }, id) {
@@ -26,7 +27,21 @@ const mutations = {
   SET_CURRENT_PRICING_TIERS: (state, tier) => (state.current_pricing_tiers = tier),
   RESET_CURRENT_PRICING_TIERS: (state) => (state.current_pricing_tiers = {}),
   ADD_PRICING_TIER: (state, tier) => state.current_pricing_tiers.unshift(tier),
-  REMOVE_PRICING_TIER: (state, id) => (state.current_pricing_tiers = state.current_pricing_tiers.filter(tier => tier.id !== id))
+  REMOVE_PRICING_TIER: (state, id) => (state.current_pricing_tiers = state.current_pricing_tiers.filter(tier => tier.id !== id)),
+  SORT_PRICING_TIERS: (state) => {
+    function compare (a, b) {
+      const tierA = a.lower_bound
+      const tierB = b.lower_bound
+      let comparison = 0
+      if (tierA > tierB) {
+        comparison = 1
+      } else if (tierA < tierB) {
+        comparison = -1
+      }
+      return comparison
+    }
+    state.current_pricing_tiers.sort(compare)
+  }
 }
 
 export default {
