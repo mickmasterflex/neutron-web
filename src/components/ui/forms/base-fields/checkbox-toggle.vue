e02f5b2cf79a885b39ba099bb238d51ec3b535d8
Kevin Bigelow <kbigelow5@gmail.com>
2020-10-20 22:41:31 -0600

    checkbox toggle component

---
+++ src/components/ui/forms/base-fields/checkbox-toggle.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <label :for="field_id" :class="`${label_class} ${checked === true ? 'border-green-500' : 'border-red-500'}`"
+           class="toggle-switch transition-colors duration-150 border-2 rounded-lg bg-white p-1 flex flex-row justify-between cursor-pointer">
+      <span :class="`switch switch-off ${checked === false ? 'active' : ''}`">
+        <slot name="switch-off">
+          <font-awesome-icon icon="ban" v-if="icons"></font-awesome-icon>
+          <span v-else>Off</span>
+        </slot>
+      </span>
+      <span :class="`switch switch-on ${checked === true ? 'active' : ''}`">
+        <slot name="switch-on">
+          <font-awesome-icon icon="check" v-if="icons"></font-awesome-icon>
+          <span v-else>On</span>
+        </slot>
+      </span>
+    </label>
+    <input
+      @input="handleInput($event.target.checked)"
+      type="checkbox"
+      :id="field_id"
+      :class="$attrs.field_class"
+      class="hidden"
+      :checked="value"
+      v-model="checked">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    field_label: {
+      type: String
+    },
+    label_class: String,
+    field_id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: [Boolean, String]
+    },
+    icons: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      checked: this.value
+    }
+  },
+  methods: {
+    handleInput (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .toggle-switch {
+    padding: .125rem;
+    height: 38px;
+    width: 90px;
+  }
+  .switch {
+    padding: .125rem;
+    @apply w-1/2 flex text-gray-700 flex-row items-center justify-center font-bold select-none transition-colors duration-150 rounded
+  }
+  .switch.active {
+    @apply text-white
+  }
+  .switch-on.active {
+    @apply bg-green-500
+  }
+  .switch-off.active {
+    @apply bg-red-500
+  }
+</style>
