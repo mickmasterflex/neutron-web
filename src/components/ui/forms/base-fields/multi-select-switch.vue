f92fcb67ca9a93ca03c5b9c5d8209755b34a1aa1
Kevin Bigelow <kbigelow5@gmail.com>
2020-10-26 11:48:15 -0600

    moving switch component updated with props, adjusting styles for large and small switches

--- src/components/ui/forms/base-fields/multi-select-switch.vue
+++ src/components/ui/forms/base-fields/multi-select-switch.vue
@@ -3,11 +3,11 @@
     <label class="field-label field-label-top" v-if="$attrs.field_label" :for="$attrs.field_id">{{$attrs.field_label}}</label>
     <div class="flex flex-col">
       <div v-for="option in options" :key="option.id" class="flex flex-row items-center mb-2">
-        <switch-toggle :for="option.name + option.id" :isChecked="selectedOption(option.id)">
+        <switch-toggle size="sm" :fieldId="option.name + option.id" :isChecked="selectedOption(option.id)">
           <template v-slot:switch-off><slot name="switch-off"/></template>
           <template v-slot:switch-on><slot name="switch-on"/></template>
         </switch-toggle>
-        <label class="ml-1">
+        <label class="pl-2 cursor-pointer">
           <input
             :id="option.name + option.id"
             class="hidden"
@@ -25,7 +25,7 @@
 
 <script>
 import multiSelectMixin from '@/mixins/fields/multi-select'
-import switchToggle from '@/components/ui/forms/functional/switch'
+import switchToggle from '@/components/ui/forms/base-fields/switch'
 
 export default {
   mixins: [multiSelectMixin],
---
+++ src/components/ui/forms/base-fields/switch.vue
@@ -0,0 +1,70 @@
+<template>
+  <label :for="fieldId"
+         :class="`toggle-switch transition-colors duration-150 border border-r-2 border-b-2 rounded-lg bg-white p-1 flex flex-row justify-between cursor-pointer
+                  ${isChecked === true ? 'border-green-500' : 'border-gray-500'}
+                  ${isChecked === false ? 'border-red-500' : 'border-gray-500'}
+                  toggle-switch-${size}`">
+    <span :class="`switch switch-off ${isChecked === false ? 'active' : ''}`">
+      <slot name="switch-off">
+        <font-awesome-icon icon="ban"></font-awesome-icon>
+      </slot>
+    </span>
+    <span :class="`switch switch-on ${isChecked === true ? 'active' : ''}`">
+      <slot name="switch-on">
+        <font-awesome-icon icon="check"></font-awesome-icon>
+      </slot>
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    isChecked: {
+      type: Boolean,
+      required: true
+    },
+    size: {
+      type: String,
+      default: 'lg',
+      validator: function (value) {
+        return ['sm', 'lg'].includes(value)
+      }
+    },
+    fieldId: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .toggle-switch-lg {
+    padding: .125rem;
+    height: 38px;
+    width: 80px;
+  }
+  .toggle-switch-sm {
+    padding: 2px;
+    height: 32px;
+    width: 66px;
+  }
+  .toggle-switch-sm .switch {
+    @apply text-sm
+  }
+  .switch {
+    padding: .125rem;
+    border-radius: 5px;
+    @apply w-1/2 flex text-gray-700 flex-row items-center justify-center font-bold select-none transition-colors duration-150
+  }
+  .switch.active {
+    @apply text-white
+  }
+  .switch-on.active {
+    @apply bg-green-500
+  }
+  .switch-off.active {
+    @apply bg-red-500
+  }
+</style>
