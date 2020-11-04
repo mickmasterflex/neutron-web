ad6c41473584fe1d4427a63fe2e82ba6618496bb
Kevin Bigelow <kbigelow5@gmail.com>
2020-11-04 12:22:53 -0700

    adding content-layout to buyer-group view

--- src/views/relationships/buyer-groups.vue
+++ src/views/relationships/buyer-groups.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>This is Buyer Groups</h1>
+  <content-layout>
+    <template v-slot:hud>
+      <h1 class="text-white text-4xl font-hairline">Buyer Groups</h1>
+      <div class="hud--stat-cards"></div>
+    </template>
+    <template v-slot:content>
+      List Buyer Groups
+    </template>
+  </content-layout>
 </template>
 
 <script>
