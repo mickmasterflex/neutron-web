4301eb3bdca37cd00b22ace41bfe88a4ffe624a1
Kevin Bigelow <kbigelow5@gmail.com>
2020-11-05 10:06:07 -0700

    indentation fix

--- src/components/buyers/create.vue
+++ src/components/buyers/create.vue
@@ -2,15 +2,15 @@
   <modal-template :show="showModal" @close="close">
     <template v-slot:header>Create Buyer Contract</template>
     <template v-slot:body>
-  <validation-observer ref="form">
-    <form @submit.prevent="submitForm" class="form-horizontal">
-      <v-text-field v-model="name" rules="required|standard_chars" field_id="name" field_label="Name"></v-text-field>
-      <v-select-field v-model="status" rules="required" :options="statusOptions" field_id="status" field_label="Status"></v-select-field>
-      <v-text-field placeholder="5.99" v-model="rpl" rules="dollar_amount|required" field_id="rpl" field_label="Rev. Per Lead"></v-text-field>
-      <select-buyer-group v-model="buyerGroup"></select-buyer-group>
-      <date-picker v-model="scheduledStart" field_id="scheduled_start" field_label="Scheduled Start"></date-picker>
-    </form>
-  </validation-observer>
+      <validation-observer ref="form">
+        <form @submit.prevent="submitForm" class="form-horizontal">
+          <v-text-field v-model="name" rules="required|standard_chars" field_id="name" field_label="Name"></v-text-field>
+          <v-select-field v-model="status" rules="required" :options="statusOptions" field_id="status" field_label="Status"></v-select-field>
+          <v-text-field placeholder="5.99" v-model="rpl" rules="dollar_amount|required" field_id="rpl" field_label="Rev. Per Lead"></v-text-field>
+          <select-buyer-group v-model="buyerGroup"></select-buyer-group>
+          <date-picker v-model="scheduledStart" field_id="scheduled_start" field_label="Scheduled Start"></date-picker>
+        </form>
+      </validation-observer>
     </template>
     <template v-slot:footer-additional>
       <button @click="submitForm()" class="btn btn-lg btn-green">Create Buyer Contract</button>
