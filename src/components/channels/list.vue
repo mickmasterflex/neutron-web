b1fee81aa4000b178b5bf52997ab0ef5f943acf5
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-11-11 13:16:17 -0700

    styling for delete in channels

--- src/components/channels/list.vue
+++ src/components/channels/list.vue
@@ -4,12 +4,15 @@
       <thead>
       <tr>
         <th class="th">Name</th>
+        <th class="th"></th>
       </tr>
       </thead>
       <tbody class="tbody">
       <tr class="tr" v-for="channel in this.channels" :key="channel.id">
         <td class="td">
           <router-link :to="{name: 'Channels', params: { id:channel.id }}" class="underline text-blue-500">{{channel.name}}</router-link>
+        </td>
+        <td class="td flex flex-row justify-end">
           <delete-channel :id="channel.id"></delete-channel>
         </td>
       </tr>
