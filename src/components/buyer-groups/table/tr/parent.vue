81120bdd11d4a27842a8dadd8b2d60176d1fd68a
Kevin Bigelow <kbigelow5@gmail.com>
2020-11-24 14:39:18 -0700

    updating some naing conventions

--- src/components/buyer-groups/table/tr/parent.vue
+++ src/components/buyer-groups/table/tr/parent.vue
@@ -2,9 +2,9 @@
   <div>
     <ul class="tr flex flex-row">
       <li class="td border-l border-b border-gray-200 w-32">
-        <span v-if="state.buyers.length" @click="toggleChildrenVisibilty()" class="p-1 mr-1 cursor-pointer">
-          <font-awesome-icon v-if="!childrenVisibility" icon="caret-right" class="text-gray-500 hover:text-gray-700"></font-awesome-icon>
-          <font-awesome-icon v-if="childrenVisibility" icon="caret-down" class="text-gray-800"></font-awesome-icon>
+        <span v-if="state.buyers.length" @click="toggleChildrenVisible()" class="p-1 mr-1 cursor-pointer">
+          <font-awesome-icon v-if="!childrenVisible" icon="caret-right" class="text-gray-500 hover:text-gray-700"></font-awesome-icon>
+          <font-awesome-icon v-if="childrenVisible" icon="caret-down" class="text-gray-800"></font-awesome-icon>
         </span>
         <slot name="checkbox">
           <checkbox-field
@@ -20,7 +20,7 @@
       <li class="td border-b border-gray-200 w-64">{{obj.name}}</li>
       <li class="td border-r border-b border-gray-200 w-32">{{obj.buyer_group}}</li>
     </ul>
-    <div v-if="childrenVisibility">
+    <div v-if="childrenVisible">
       <buyer-node v-for="buyer in state.buyers" :obj="buyer" :key="buyer.id" class="pl-3"></buyer-node>
     </div>
   </div>
@@ -30,19 +30,19 @@
 import parent from '@/components/buyer-groups/table/tr/parent'
 import { ref, reactive, computed } from '@vue/composition-api'
 
-function buyerChildrenVisibility () {
-  const childrenVisibility = ref(false)
+function useChildVisibility () {
+  const childrenVisible = ref(false)
 
-  function toggleChildrenVisibilty () {
-    childrenVisibility.value = !childrenVisibility.value
+  function toggleChildrenVisible () {
+    childrenVisible.value = !childrenVisible.value
   }
   return {
-    childrenVisibility,
-    toggleChildrenVisibilty
+    childrenVisible,
+    toggleChildrenVisible
   }
 }
 
-function client (root, clientId) {
+function useClient (root, clientId) {
   const state = reactive({
     buyers: computed(
       () => root.$store.getters.getParentlessBuyersByClient(clientId)
@@ -93,7 +93,7 @@ function client (root, clientId) {
   }
 }
 
-function buyer (root, buyerId) {
+function useBuyer (root, buyerId) {
   const state = reactive({
     buyer: computed(
       () => root.$store.getters.getBuyerById(buyerId)
@@ -153,20 +153,20 @@ export default {
     }
   },
   setup (props, { root }) {
-    const { childrenVisibility, toggleChildrenVisibilty } = buyerChildrenVisibility()
+    const { childrenVisible, toggleChildrenVisible } = useChildVisibility()
 
     const currentBuyerGroup = computed(() => root.$store.getters.getCurrentBuyerGroup)
     const { check, checkboxState, state } =
       props.type === 'client'
-        ? client(root, props.obj.id, currentBuyerGroup.value)
-        : buyer(root, props.obj.id, currentBuyerGroup.value)
+        ? useClient(root, props.obj.id, currentBuyerGroup.value)
+        : useBuyer(root, props.obj.id, currentBuyerGroup.value)
 
     return {
       check,
       checkboxState,
-      childrenVisibility,
+      childrenVisible,
       state,
-      toggleChildrenVisibilty
+      toggleChildrenVisible
     }
   },
   components: {
