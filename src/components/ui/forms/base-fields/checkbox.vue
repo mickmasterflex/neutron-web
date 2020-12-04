bc4c55b44f10b254d4c96cad43a3c05174efb6a6
Kevin Bigelow <kbigelow5@gmail.com>
2020-12-04 16:03:30 -0700

    checkbox component

---
+++ src/components/ui/forms/base-fields/checkbox.vue
@@ -0,0 +1,62 @@
+<template>
+  <label :for="field_id" :class="showDisabled ? 'cursor-not-allowed' : 'cursor-pointer'">
+    <font-awesome-icon v-if="indeterminate"
+                       icon="minus-square"
+                       class="text-yellow-500 hover:text-yellow-600"
+    ></font-awesome-icon>
+    <font-awesome-icon v-else-if="value"
+                       icon="check-square"
+                       :class="{'text-blue-300': showDisabled,
+                                'text-blue-500 hover:text-blue-600': !showDisabled}"
+    ></font-awesome-icon>
+    <font-awesome-icon v-else
+                       :icon="['far', 'square']"
+                       :class="{'text-gray-300': showDisabled,
+                                'text-gray-600 hover:text-gray-900': !showDisabled}"
+    ></font-awesome-icon>
+    <input
+      ref="checkbox"
+      @input="$emit('input', $event.target.checked)"
+      type="checkbox"
+      :id="field_id"
+      class="hidden"
+      :class="$attrs.field_class"
+      :checked="value"
+      :indeterminate.prop="indeterminate"
+      :disabled="disabled">
+    {{field_label}}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    field_label: {
+      type: String
+    },
+    field_id: {
+      type: [String, Number],
+      required: true
+    },
+    value: {
+      type: [Boolean, String]
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    disabledVisually: {
+      type: Boolean,
+      default: false
+    },
+    indeterminate: {
+      type: Boolean
+    }
+  },
+  computed: {
+    showDisabled () {
+      return this.disabled || this.disabledVisually
+    }
+  }
+}
+</script>
