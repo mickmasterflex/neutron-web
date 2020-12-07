87cc82ddedde2b45aff57a055d30583a0fffe887
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-12-07 16:21:22 -0700

    table-button-link

---
+++ src/components/ui/buttons/table-link.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link class="rounded-lg border-gray-900"
+   :to="route"><slot/>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    route: Object
+  }
+}
+</script>
