5534b208d7dbfb20ad2d6b896c777a64490577fb
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2020-12-14 16:12:57 -0700

    making object required in object

--- src/components/ui/buttons/table-link.vue
+++ src/components/ui/buttons/table-link.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="hover:bg-gray-600 rounded-lg border-b-3 border-r-3 border-2 px-2 py-1 border-gray-900"
+  <router-link class="transition duration-500 ease-in-out bg-white-500 hover:bg-gray-600 transform hover:-translate-y-1 hover:scale-110 rounded-lg border-b-2 border-r-2 border-t border-l px-2 py-1 border-gray-800"
    :to="route"><slot/>
   </router-link>
 </template>
