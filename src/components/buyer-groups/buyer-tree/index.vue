55adee25d4dd2e1b091baec4ae83ca690ead88c7
Kevin Bigelow <kbigelow5@gmail.com>
2020-12-14 17:30:38 -0700

    loading overlay position and transition refine

--- src/components/buyer-groups/buyer-tree/index.vue
+++ src/components/buyer-groups/buyer-tree/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full relative">
+  <div class="w-full">
     <div v-if="allClients.length > 0 && allBuyers.length > 0">
       <ul class="flex flex-row font-normal p-3 rounded-t-lg border-2 border-gray-200 border-b-0 bg-gray-900 text-left text-white">
         <li class="w-64 px-3">Name</li>
@@ -7,8 +7,10 @@
         <li class="w-24 px-3">Status</li>
         <li class="w-20 px-3">Buyers</li>
       </ul>
-      <div class="bg-white p-2 rounded-b-lg border-2 border-t-0 border-gray-200" :class="{'cursor-wait': loading}">
-        <div v-show="loading" class="absolute inset-0 flex flex-row items-center justify-center bg-white bg-opacity-50 z-50"></div>
+      <div class="bg-white p-2 rounded-b-lg border-2 border-t-0 border-gray-200 relative" :class="{'cursor-wait': loading}">
+        <transition enter-active-class="animate__animated animate__fadeIn animate__faster" leave-active-class="animate__animated animate__fadeOut animate__faster">
+          <div v-show="loading" class="absolute inset-0 flex flex-row items-center justify-center bg-white bg-opacity-75 z-50"></div>
+        </transition>
         <buyer-tree-node
           v-for="client in allClients"
           :key="client.id"
