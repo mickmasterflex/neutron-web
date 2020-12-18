27bb97347062580c447da194103185036522c679
Kevin Bigelow <kbigelow5@gmail.com>
2020-12-18 11:02:30 -0700

    creating a text-field-suffixed component

---
+++ src/components/ui/forms/base-fields/text-field-suffixed.vue
@@ -0,0 +1,9 @@
+<template>
+  <text-field :field_class="$attrs.field_class + ` w-56 pr-8`"
+              :field_wrap_class="$attrs.field_wrap_class + ` flex flex-row items-center`"
+              v-bind="$attrs">
+    <span class="ml-2">
+      <slot></slot>
+    </span>
+  </text-field>
+</template>
