35bf7f80723b377b0067d36481f077035311bc0b
Kevin Bigelow <kbigelow5@gmail.com>
2020-12-22 11:59:54 -0700

    comma dangle fix

--- src/store/modules/forms/base-fields/visibility.js
+++ src/store/modules/forms/base-fields/visibility.js
@@ -4,12 +4,12 @@ const state = {
 }
 
 const getters = {
-  getShowCreateBaseFieldModal: state => state.show_create_base_field_modal,
+  getShowCreateBaseFieldModal: state => state.show_create_base_field_modal
 }
 
 const mutations = {
   SHOW_CREATE_BASE_FIELD_MODAL: (state) => (state.show_create_base_field_modal = true),
-  CLOSE_CREATE_BASE_FIELD_MODAL: (state) => (state.show_create_base_field_modal = false),
+  CLOSE_CREATE_BASE_FIELD_MODAL: (state) => (state.show_create_base_field_modal = false)
 }
 
 export default {
