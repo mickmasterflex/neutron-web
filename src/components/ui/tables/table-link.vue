20cd25f6ef8627ae3a1c31d4cb978f25724cfce6
Kevin Bigelow <kbigelow5@gmail.com>
2020-12-22 10:25:30 -0700

    style refinement

--- src/components/ui/tables/table-link.vue
+++ src/components/ui/tables/table-link.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="transition duration-500 ease-in-out bg-white-500 hover:bg-gray-600 hover:text-white rounded-lg border-b-2 border-r-2 border-t border-l px-2 py-1 border-gray-800"
+  <router-link class="transition duration-200 ease-in-out bg-white-500 hover:bg-gray-700 hover:text-white rounded-lg border-b-2 border-r-2 border-t border-l px-2 py-1 border-gray-800"
    :to="route"><slot/>
   </router-link>
 </template>
