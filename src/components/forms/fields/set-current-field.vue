81236bbb51f02c363916e8aae5d23103a332c732
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-08 13:56:32 -0700

    no longer fetching fields on click of edit button, simply setting it based off the field clicked

---
+++ src/components/forms/fields/set-current-field.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="btn btn-circle btn-hollow-blue" @click="setCurrent(field)">
+    <font-awesome-icon :icon="icon"></font-awesome-icon>
+  </button>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    field: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setCurrentField: 'SET_CURRENT_FIELD',
+      setCurrentOptions: 'SET_CURRENT_OPTIONS',
+      setInactiveOptions: 'SET_INACTIVE_OPTIONS',
+      sortCurrentOptions: 'SORT_CURRENT_OPTIONS'
+    }),
+    setCurrent (field) {
+      this.setCurrentField(field)
+      if (field.type === 'select' || field.type === 'radio') {
+        this.setCurrentOptions(field.options)
+        this.setInactiveOptions(field.inactive_options)
+        this.sortCurrentOptions()
+      }
+    }
+  }
+}
+</script>
