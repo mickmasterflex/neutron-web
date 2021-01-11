8685ff34a9fb434912e45e8409dd50ffb0b1e696
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-11 13:01:43 -0700

    adding phone type constants

--- src/store/constants/index.js
+++ src/store/constants/index.js
@@ -1,9 +1,19 @@
 import verticals from '@/store/constants/verticals'
 
+const state = {
+  phone_types: ['home', 'work', 'mobile', 'fax']
+}
+
+const getters = {
+  getPhoneTypes: state => state.phone_types
+}
+
 const modules = {
   verticals
 }
 
 export default {
+  state,
+  getters,
   modules
 }
