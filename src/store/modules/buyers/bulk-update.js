9e38bc1e769f5bd6d1db46b89f06f4478e0ac56e
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-13 16:00:44 -0700

    adding bulk-update module to buyers

---
+++ src/store/modules/buyers/bulk-update.js
@@ -0,0 +1,27 @@
+import axios from '@/axios'
+
+const state = {
+  bulk_update_buyers: []
+}
+
+const actions = {
+  async bulkUpdateBuyerStatus (status) {
+    await axios.post('/update-status/buyers/', { status: status, contracts: state.bulk_update_buyers })
+  }
+}
+
+const mutations = {
+  ADD_BUYER_TO_BULK_UPDATE: (state, buyer) => {
+    state.bulk_update_buyers.unshift(buyer)
+  },
+  REMOVE_BUYER_TO_BULK_UPDATE: (state, buyer) => {
+    state.bulk_update_buyers = state.bulk_update_buyers.filter(b => b !== buyer)
+  },
+  RESET_BULK_UPDATE_BUYERS: (state) => (state.bulk_update_buyers = [])
+}
+
+export default {
+  state,
+  actions,
+  mutations
+}
