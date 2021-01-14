348682fe8269cea7885fdd8f419a68f83ecaaba7
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-14 10:32:46 -0700

    checkbox component rigged up to state

--- src/store/modules/buyers/bulk-update.js
+++ src/store/modules/buyers/bulk-update.js
@@ -4,6 +4,10 @@ const state = {
   bulk_update_buyers: []
 }
 
+const getters = {
+  getBulkUpdateBuyers: state => state.bulk_update_buyers
+}
+
 const actions = {
   async bulkUpdateBuyerStatus (status) {
     await axios.post('/update-status/buyers/', { status: status, contracts: state.bulk_update_buyers })
@@ -14,7 +18,7 @@ const mutations = {
   ADD_BUYER_TO_BULK_UPDATE: (state, buyer) => {
     state.bulk_update_buyers.unshift(buyer)
   },
-  REMOVE_BUYER_TO_BULK_UPDATE: (state, buyer) => {
+  REMOVE_BUYER_FROM_BULK_UPDATE: (state, buyer) => {
     state.bulk_update_buyers = state.bulk_update_buyers.filter(b => b !== buyer)
   },
   RESET_BULK_UPDATE_BUYERS: (state) => (state.bulk_update_buyers = [])
@@ -22,6 +26,7 @@ const mutations = {
 
 export default {
   state,
+  getters,
   actions,
   mutations
 }
