1c6dfcc49e839d6f9f9181791bf4d5558b89d931
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2021-01-22 09:41:37 -0700

    fixing delete on details

--- src/views/products/education-product/details.vue
+++ src/views/products/education-product/details.vue
@@ -1,9 +1,11 @@
 <template>
   <base-panel-grid>
     <update-product :educationProduct="educationProduct" class="col-span-2 xl:col-span-1"></update-product>
+    <panel-template title="Danger Zone" class="col-span-2" :showLoader="loading" :loadingText="loadingText">
       <template #content>
         <delete-education-product :educationProduct="educationProduct"></delete-education-product>
       </template>
+    </panel-template>
   </base-panel-grid>
 </template>
 
@@ -15,7 +17,9 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters({
-      educationProduct: 'getCurrentEducationProduct'
+      educationProduct: 'getCurrentEducationProduct',
+      loading: 'getEducationProductFetchLoading',
+      loadingText: 'getEducationProductFetchLoadingText'
     })
   },
   components: {
