34aad99b2b32db637fa6d8dcc77a3e870d77ea68
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-27 13:32:05 -0700

    hud stat cards component

---
+++ src/components/ui/hud-stat-cards.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="col-auto items-center border-l border-gray-800 pl-4">
+    <transition-group enter-active-class="animate__animated animate__fadeIn">
+      <slot></slot>
+    </transition-group>
+  </div>
+</template>
