544ecc481269014d98c99a00d7f06299864d9dc4
Kevin Bigelow <kbigelow5@gmail.com>
2021-01-28 15:01:49 -0700

    leads list

---
+++ src/components/analytics/leads-list.vue
@@ -0,0 +1,49 @@
+<template>
+  <transition-table-state>
+    <table v-if="leads.length" class="table table-striped">
+      <thead>
+      <tr>
+        <th class="th">Lead Id</th>
+        <th class="th">Lead Email</th>
+        <th class="th">Campaign</th>
+        <th class="th">Revenue</th>
+        <th class="th">Margin</th>
+        <th class="th">Payout</th>
+        <th class="th">Scrub Reason</th>
+        <th class="th">Sold</th>
+        <th class="th">Created At</th>
+        <th class="th">Updated At</th>
+      </tr>
+      </thead>
+      <tbody class="tbody">
+      <tr class="tr" v-for="lead in leads" :key="lead.id">
+        <td class="td">{{lead.id}}</td>
+        <td class="td">{{lead.data.email}}</td>
+        <td class="td">{{lead.campaign}}</td>
+        <td class="td">{{lead.revenue}}</td>
+        <td class="td">{{lead.margin}}</td>
+        <td class="td">{{lead.payout}}</td>
+        <td class="td">{{lead.reason}}</td>
+        <td class="td">{{lead.sold_at}}</td>
+        <td class="td">{{lead.created_at}}</td>
+        <td class="td">{{lead.updated_at}}</td>
+      </tr>
+      </tbody>
+    </table>
+    <table-empty-state v-else
+                       heading="No leads in selected date range"
+                       key="empty"
+                       copy="Try altering the date range"></table-empty-state>
+  </transition-table-state>
+</template>
+
+<script>
+export default {
+  props: {
+    leads: {
+      type: [Object, Array],
+      required: true
+    }
+  }
+}
+</script>
--- src/views/analytics/buyer-stats/clients/leads.vue
+++ src/views/analytics/buyer-stats/clients/leads.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Bring me leads, and fire.</h1>
+  <panel-template
+    :actionTransition="true"
+    :showLoader="loading"
+    :loadingText="loadingText">
+    <template slot="title">
+      <span class="flex flex-row items-center bg-gray-200 rounded-full p-1">
+        <button class="btn-blue btn btn-circle btn-md">
+          <font-awesome-icon icon="calendar-alt"></font-awesome-icon>
+        </button>
+        <span class="font-bold px-2">
+          {{ dateRangeText }}
+        </span>
+      </span>
+    </template>
+    <template slot="content">
+      <leads-list :leads="leads"></leads-list>
+    </template>
+  </panel-template>
 </template>
+
+<script>
+import leadsList from '@/components/analytics/leads-list'
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      leads: 'getBuyerClientsLeads',
+      loading: 'getBuyerClientsFetchLoading',
+      loadingText: 'getBuyerClientsFetchLoadingText',
+      dateRangeText: 'getAnalyticsDateRange'
+    })
+  },
+  components: {
+    'leads-list': leadsList
+  }
+}
+</script>
