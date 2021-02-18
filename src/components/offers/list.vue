980a93f137639e6b5ad3dffae095faabe1f189da
Emily Jonaitis <emilyjonaitis@Emilys-MacBook-Pro.local>
2021-02-17 17:01:53 -0700

    adding set current form/fields

--- src/components/offers/list.vue
+++ src/components/offers/list.vue
@@ -55,10 +55,14 @@ export default {
   },
   methods: {
     ...mapMutations({
-      setCurrentOffer: 'SET_CURRENT_OFFER'
+      setCurrentOffer: 'SET_CURRENT_OFFER',
+      setCurrentForm: 'SET_CURRENT_FORM',
+      sortCurrentFormFields: 'SORT_CURRENT_FORM_FIELDS'
     }),
     linkToOffer (offer) {
       this.setCurrentOffer(offer)
+      this.setCurrentForm(offer.form)
+      this.sortCurrentFormFields()
       this.$router.push({ name: 'OfferDetails', params: { client: this.client, buyer: offer.parent, id: offer.id } })
     }
   }
