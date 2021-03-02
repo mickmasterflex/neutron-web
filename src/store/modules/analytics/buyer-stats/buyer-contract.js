327b50e44f9fb99f65c6f68b53b05356ced54814
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-02 16:19:11 -0700

    getting contracts tab in place for buyer and partner analytics views

--- src/store/modules/analytics/buyer-stats/buyer-contract.js
+++ src/store/modules/analytics/buyer-stats/buyer-contract.js
@@ -3,12 +3,14 @@ import axios from '@/axios'
 const state = {
   current_buyer_stats_contract: {},
   buyer_contract_stats_offers: [],
+  buyer_contract_stats_contracts: [],
   buyer_contract_stats_fetch_loading_text: 'Loading Buyer Contract Data'
 }
 
 const getters = {
   getCurrentBuyerStatsContract: state => state.current_buyer_stats_contract,
-  getBuyerContractStatsOffers: state => state.buyer_contract_stats_offers
+  getBuyerContractStatsOffers: state => state.buyer_contract_stats_offers,
+  getBuyerContractStatsContracts: state => state.buyer_contract_stats_contracts
 }
 
 const actions = {
@@ -18,6 +20,7 @@ const actions = {
     await axios.get(`/analytics/offer-contracts/?${getters.getAnalyticsDateRangeUrlFormatted}&buyer_contract=${id}`)
       .then(response => {
         commit('SET_BUYER_CONTRACT_STATS_OFFERS', response.data.offer_contracts)
+        commit('SET_BUYER_CONTRACT_STATS_CONTRACTS', response.data.buyer_contracts)
         commit('SET_ANALYTICS_TOTALS', response.data.totals)
         commit('SET_ANALYTICS_LEADS', response.data.leads)
         commit('SET_CURRENT_BUYER_STATS_CONTRACT', response.data.contract)
@@ -30,7 +33,8 @@ const actions = {
 
 const mutations = {
   SET_CURRENT_BUYER_STATS_CONTRACT: (state, contract) => (state.current_buyer_stats_contract = contract),
-  SET_BUYER_CONTRACT_STATS_OFFERS: (state, buyerOffers) => (state.buyer_contract_stats_offers = buyerOffers)
+  SET_BUYER_CONTRACT_STATS_OFFERS: (state, buyerOffers) => (state.buyer_contract_stats_offers = buyerOffers),
+  SET_BUYER_CONTRACT_STATS_CONTRACTS: (state, contracts) => (state.buyer_contract_stats_contracts = contracts)
 }
 
 export default {
