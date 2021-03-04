78a05a0548777eee38b5430c18df68b4ca11be77
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-04 12:49:34 -0700

    adding breadcrumbs with ancestors to buyer contract stats pages, adding key to component tied to router id to force component rerender

--- src/views/analytics/contract/buyer/buyer-contract-layout.vue
+++ src/views/analytics/contract/buyer/buyer-contract-layout.vue
@@ -8,6 +8,7 @@
                      query: $route.query
                      }"
                    :fetchContractStats="fetchBuyerContractStats"
+                   :key="$route.params.id"
   >
     <template v-slot:statCards>
       <stat-card :data="offers.length" title="Offers" key="offerCountStatCard" ></stat-card>
@@ -33,22 +34,89 @@
 
 <script>
 import contractLayout from '@/views/analytics/contract/layout'
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 
 export default {
   props: {
     clientId: Number,
     id: Number
   },
+  data () {
+    return {
+      BuyerStatsClients: {
+        name: 'BuyerStatsClients',
+        text: 'All Clients',
+        query: this.$route.query
+      },
+      BuyerStatsClientContracts: {
+        name: 'BuyerStatsClientContracts',
+        text: this.$route.params.clientId,
+        params: { id: this.$route.params.clientId },
+        query: this.$route.query
+      },
+      BuyerStatsContract: {
+        name: 'BuyerStatsContract',
+        text: this.$route.params.id,
+        params: {
+          clientId: this.$route.params.clientId,
+          id: this.$route.params.id
+        },
+        query: this.$route.query
+      }
+    }
+  },
   computed: {
     ...mapGetters({
       contract: 'getCurrentBuyerStatsContract',
-      offers: 'getBuyerContractStatsOffers'
+      buyerClient: 'getCurrentBuyerStatsClient',
+      offers: 'getBuyerContractStatsOffers',
+      ancestors: 'getBuyerContractStatsAncestors',
+      loading: 'getAnalyticsFetchLoading'
     })
   },
+  watch: {
+    contract () {
+      this.BuyerStatsContract.text = this.contract.name
+    },
+    buyerClient () {
+      this.BuyerStatsClientContracts.text = this.buyerClient.name
+    },
+    loading () {
+      this.setBreadcrumbsWithAncestors()
+    }
+  },
+  created () {
+    this.setBreadcrumbs([
+      this.BuyerStatsClients,
+      this.BuyerStatsClientContracts,
+      this.BuyerStatsContract
+    ])
+  },
   methods: {
+    setBreadcrumbsWithAncestors () {
+      const breadcrumbs = [
+        this.BuyerStatsClients,
+        this.BuyerStatsClientContracts
+      ]
+      this.ancestors.forEach(ancestor => {
+        breadcrumbs.push({
+          name: 'BuyerStatsContract',
+          text: ancestor.name,
+          params: {
+            clientId: this.$route.params.clientId,
+            id: ancestor.id
+          },
+          query: this.$route.query
+        })
+      })
+      breadcrumbs.push(this.BuyerStatsContract)
+      this.setBreadcrumbs(breadcrumbs)
+    },
     ...mapActions({
       fetchBuyerContractStats: 'fetchBuyerContractStats'
+    }),
+    ...mapMutations({
+      setBreadcrumbs: 'SET_CURRENT_BREADCRUMBS'
     })
   },
   components: {
