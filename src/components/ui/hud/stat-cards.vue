6c1d5f8ad3a35c92a3b6e3bc7cbf956d2accd832
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-09 21:13:20 -0700

    hud stat cards justify end

--- src/components/ui/hud/stat-cards.vue
+++ src/components/ui/hud/stat-cards.vue
@@ -2,7 +2,7 @@
   <div class="col-auto xl:col-span-2">
     <transition-group enter-active-class="animate__animated animate__fadeIn"
                       leave-active-class="animate__animated animate__fadeOut animate__faster"
-                      class="flex flex-row flex-wrap items-end justify-start"
+                      class="flex flex-row flex-wrap items-end justify-end"
     >
       <slot></slot>
     </transition-group>
