5a681f9393442e9de1758705d2d6ac518dffa1ff
Emily Jonaitis <emilyfjonaitis@gmail.com>
2021-03-12 11:06:27 -0700

    caps cal on offer details

--- src/views/relationships/contract-mgmt/offer/details.vue
+++ src/views/relationships/contract-mgmt/offer/details.vue
@@ -2,6 +2,11 @@
   <base-panel-grid>
     <update-offer :offer="offer" class="col-span-2 xl:col-span-1"></update-offer>
     <recruitment-locations :geo="offer.geo" class="col-span-2 xl:col-span-1"></recruitment-locations>
+    <panel-template title="Lead Caps" contentClass="relative" class="col-span-2">
+      <template v-slot:content>
+        <lead-caps :parent="id" type="offer-contracts"></lead-caps>
+      </template>
+    </panel-template>
     <panel-template title="Danger Zone" :showLoader="loading" :loadingText="loadingText" class="col-span-2">
       <template #content>
         <delete-offer :offer="offer" :client="$route.params.client"></delete-offer>
@@ -14,6 +19,7 @@ import { mapGetters } from 'vuex'
 import recruitmentLocations from '@/components/geos/index'
 import deleteOffer from '@/components/offers/delete'
 import updateOffer from '@/components/offers/update'
+import leadCaps from '@/components/caps/'
 
 export default {
   props: {
@@ -24,7 +30,8 @@ export default {
   components: {
     'delete-offer': deleteOffer,
     'update-offer': updateOffer,
-    'recruitment-locations': recruitmentLocations
+    'recruitment-locations': recruitmentLocations,
+    'lead-caps': leadCaps
   },
   computed: {
     ...mapGetters({
