424727f6615393f826ec242f7c04d7bec6b66384
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-22 14:10:43 -0600

    adding method to retrieve descendant contract data, or return contract data

--- src/components/analytics/contracts-list.vue
+++ src/components/analytics/contracts-list.vue
@@ -19,16 +19,16 @@
             <span class="text-link" @click="linkToContract({ name: contract.name, id: contract.id })">{{contract.name}}</span>
           </td>
           <td class="td">
-            <table-link :number="descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].lead_count : contract.lead_count"
+            <table-link :number="getDescendantData(contract, 'lead_count')"
                         @table-link-click="linkToContractLeads({ name: contract.name, id: contract.id })"
             ></table-link>
           </td>
-          <td-number :number="descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].sold_count : contract.sold_count"></td-number>
-          <td-number :dollar="true" :number="descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].revenue : contract.revenue"></td-number>
-          <td-number :dollar="true" :number="descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].margin : contract.margin"></td-number>
-          <td-number :dollar="true" :number="descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].payout : contract.payout"></td-number>
-          <td class="td">{{descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].scrub_rate : contract.scrub_rate}}</td>
-          <td class="td">{{descendantContractDataById(contract.id)[0] ? descendantContractDataById(contract.id)[0].margin_percent : contract.margin_percent}}</td>
+          <td-number :number="getDescendantData(contract, 'sold_count')"></td-number>
+          <td-number :dollar="true" :number="getDescendantData(contract, 'revenue')"></td-number>
+          <td-number :dollar="true" :number="getDescendantData(contract, 'margin')"></td-number>
+          <td-number :dollar="true" :number="getDescendantData(contract, 'payout')"></td-number>
+          <td class="td">{{getDescendantData(contract, 'scrub_rate')}}</td>
+          <td class="td">{{getDescendantData(contract, 'margin_percent')}}</td>
         </tr>
       </tbody>
     </table>
@@ -102,6 +102,12 @@ export default {
         params: { id: contract.id, clientId: this.client.id },
         query: this.$route.query
       })
+    },
+    getDescendantData (contract, key) {
+      if (this.descendantContractDataById(contract.id)[0]) {
+        return this.descendantContractDataById(contract.id)[0][key]
+      }
+      return contract[key]
     }
   }
 }
