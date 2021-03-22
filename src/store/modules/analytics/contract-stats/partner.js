237d6738d176df1de93df8d5d36f6fe53662cbc1
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-22 12:15:28 -0600

    abstracting shared contract commits into an action

--- src/store/modules/analytics/contract-stats/partner.js
+++ src/store/modules/analytics/contract-stats/partner.js
@@ -5,18 +5,13 @@ const state = {
 }
 
 const actions = {
-  async fetchPartnerContractStats ({ commit, getters }, id) {
+  async fetchPartnerContractStats ({ commit, getters, dispatch }, id) {
     commit('SET_ANALYTICS_FETCH_LOADING_TEXT', state.partner_contract_stats_fetch_loading_text)
     commit('SET_ANALYTICS_FETCH_LOADING')
     await axios.get(`/analytics/campaigns/?${getters.getAnalyticsDateRangeUrlFormatted}&partner_contract=${id}`)
       .then(response => {
-        commit('SET_CURRENT_STATS_DESCENDANT_CONTRACT_DATA', response.data.descendant_contract_data)
-        commit('SET_CURRENT_STATS_CONTRACTS', response.data.descendants)
         commit('SET_CURRENT_STATS_CAMPAIGNS', response.data.campaigns)
-        commit('SET_ANALYTICS_TOTALS', response.data.totals)
-        commit('SET_ANALYTICS_LEADS', response.data.leads)
-        commit('SET_CURRENT_CONTRACT_STATS', response.data.contract)
-        commit('SET_CURRENT_CLIENT_STATS', response.data.client)
+        dispatch('setCurrentContractData', response.data)
       }).finally(() => {
         commit('RESET_ANALYTICS_FETCH_LOADING')
       })
