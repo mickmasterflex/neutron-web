3a8f5cbebaaf28b21fcb5b112bde493affafebfb
Kevin Bigelow <kbigelow5@gmail.com>
2021-03-30 14:55:52 -0600

    tr-td-section-heading component

---
+++ src/components/ui/tables/tr-td-section-heading.vue
@@ -0,0 +1,19 @@
+<template>
+  <tr class="tr">
+    <td :colspan="colspan"
+        class="font-bold px-3 py-3 bg-gray-300">
+      <slot></slot>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    colspan: {
+      default: 1,
+      type: Number
+    }
+  }
+}
+</script>
