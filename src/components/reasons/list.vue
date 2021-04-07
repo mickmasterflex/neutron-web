b6c02fbbf741d49e3af5928863dd661e7c3b5164
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-07 10:52:44 -0600

    restricting width

--- src/components/reasons/list.vue
+++ src/components/reasons/list.vue
@@ -6,7 +6,7 @@
         <th class="th w-16">Id</th>
         <th class="th">Name</th>
         <th class="th">Description</th>
-        <th class="th"></th>
+        <th class="th w-20"></th>
       </tr>
       </thead>
       <tbody class="tbody">
