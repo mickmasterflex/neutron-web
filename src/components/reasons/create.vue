4637cc194b740add4fdeca37122ca63846223a9d
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-07 12:43:11 -0600

    abstracting create and update reason common elements into reasons/modal.vue

--- src/components/reasons/create.vue
+++ src/components/reasons/create.vue
@@ -1,43 +1,20 @@
 <template>
-  <modal-template :show="modalVisible" @close="close">
-    <template v-slot:header>Create Reason</template>
-    <template v-slot:body>
-      <validation-observer ref="form">
-        <form @submit.prevent="submitForm" class="form-horizontal">
-          <v-text-field
-            v-model="name"
-            rules="required|standard_chars"
-            field_id="name"
-            field_label="Name"
-            :field_disabled="loading"></v-text-field>
-          <v-textarea-field
-            v-model="description"
-            rules="required|standard_chars"
-            field_id="description"
-            field_label="Description"
-            :field_disabled="loading"></v-textarea-field>
-        </form>
-      </validation-observer>
-    </template>
-    <template v-slot:footer-additional>
-      <button @click="submitForm()" class="btn btn-lg btn-green" :disabled="loading">
-        <font-awesome-icon v-if="loading" icon="spinner" pulse></font-awesome-icon> Create Reason
-      </button>
-    </template>
-  </modal-template>
+  <reason-modal
+    :modal-visible="modalVisible"
+    :loading="loading"
+    :submit-action="createReason"
+    :close-modal="closeModal"
+    modal-purpose="Create"
+  ></reason-modal>
 </template>
 
 <script>
+import reasonModal from '@/components/reasons/modal'
 import { mapActions, mapGetters, mapMutations } from 'vuex'
-import { enterKeyListener } from '@/mixins/enter-key-listener'
-import { setResponseErrors } from '@/mixins/set-response-errors'
 
 export default {
-  data () {
-    return {
-      name: '',
-      description: ''
-    }
+  components: {
+    'reason-modal': reasonModal
   },
   computed: {
     ...mapGetters({
@@ -45,36 +22,13 @@ export default {
       loading: 'getReasonsPostLoading'
     })
   },
-  mixins: [enterKeyListener, setResponseErrors],
   methods: {
     ...mapActions({
       createReason: 'createReason'
     }),
     ...mapMutations({
       closeModal: 'CLOSE_CREATE_REASON_MODAL'
-    }),
-    close () {
-      this.name = ''
-      this.description = ''
-      this.$nextTick(() => {
-        this.$refs.form.reset()
-      })
-      this.closeModal()
-    },
-    submitForm () {
-      this.$refs.form.validate().then(success => {
-        if (success) {
-          this.createReason({
-            name: this.name,
-            description: this.description
-          }).then(() => {
-            this.close()
-          }).catch(error => {
-            this.error = error
-          })
-        }
-      })
-    }
+    })
   }
 }
 </script>
--- src/components/reasons/update.vue
+++ src/components/reasons/update.vue
@@ -1,64 +1,26 @@
 <template>
-  <modal-template :show="modalVisible" @close="close">
-    <template v-slot:header>Update Reason</template>
-    <template v-slot:body>
-      <validation-observer ref="form">
-        <form @submit.prevent="submitForm" class="form-horizontal">
-          <v-text-field
-            v-model="name"
-            rules="required|standard_chars"
-            field_id="name"
-            field_label="Name"
-            :field_disabled="loading"></v-text-field>
-          <v-textarea-field
-            v-model="description"
-            rules="required|standard_chars"
-            field_id="description"
-            field_label="Description"
-            :field_disabled="loading"></v-textarea-field>
-        </form>
-      </validation-observer>
-    </template>
-    <template v-slot:footer-additional>
-      <button @click="submitForm()" class="btn btn-lg btn-green" :disabled="loading">
-        <font-awesome-icon v-if="loading" icon="spinner" pulse></font-awesome-icon> Update Reason
-      </button>
-    </template>
-  </modal-template>
+  <reason-modal
+    :modal-visible="modalVisible"
+    :loading="loading"
+    :submit-action="updateReason"
+    :close-modal="closeModalAndReset"
+    modal-purpose="Update"
+  ></reason-modal>
 </template>
 
 <script>
+import reasonModal from '@/components/reasons/modal'
 import { mapActions, mapGetters, mapMutations } from 'vuex'
-import { enterKeyListener } from '@/mixins/enter-key-listener'
-import { setResponseErrors } from '@/mixins/set-response-errors'
-import { checkUnsavedChangesInModal } from '@/mixins/check-unsaved-changes-in-modal'
 
 export default {
-  data () {
-    return {
-      name: '',
-      description: ''
-    }
+  components: {
+    'reason-modal': reasonModal
   },
   computed: {
     ...mapGetters({
       modalVisible: 'getShowUpdateReasonModal',
-      currentReason: 'getCurrentReason',
       loading: 'getReasonsPutLoading'
-    }),
-    unsavedChanges () {
-      if (this.name) {
-        return this.name !== this.currentReason.name || this.description !== this.currentReason.description
-      } else {
-        return false
-      }
-    }
-  },
-  mixins: [enterKeyListener, setResponseErrors, checkUnsavedChangesInModal],
-  watch: {
-    unsavedChanges () {
-      this.checkUnsavedChanges(this.modalVisible, this.unsavedChanges)
-    }
+    })
   },
   methods: {
     ...mapActions({
@@ -68,36 +30,9 @@ export default {
       resetCurrent: 'RESET_CURRENT_REASON',
       closeModal: 'CLOSE_UPDATE_REASON_MODAL'
     }),
-    close () {
+    closeModalAndReset () {
       this.closeModal()
-      this.name = ''
-      this.description = ''
       this.resetCurrent()
-      this.toggleChangesInModalUnsaved(false)
-      this.$nextTick(() => {
-        this.$refs.form.reset()
-      })
-    },
-    submitForm () {
-      this.$refs.form.validate().then(success => {
-        if (success) {
-          this.updateReason({
-            id: this.currentReason.id,
-            name: this.name,
-            description: this.description
-          }).then(() => {
-            this.close()
-          }).catch(error => {
-            this.error = error
-          })
-        }
-      })
-    }
-  },
-  updated () {
-    if (this.currentReason) {
-      this.name = this.currentReason.name
-      this.description = this.currentReason.description
     }
   }
 }
