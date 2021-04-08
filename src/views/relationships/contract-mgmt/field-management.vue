2a5f417a71f325cb98ed14d3ce16f54c3b707046
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-08 15:04:34 -0600

    abstracting fields and injected field panels into components

--- src/views/relationships/contract-mgmt/field-management.vue
+++ src/views/relationships/contract-mgmt/field-management.vue
@@ -1,58 +1,29 @@
 <template>
   <base-panel-grid>
-    <panel-template
-      title="Contract Fields"
-      :actionTransition="true"
-      :showLoader="loading"
-      :loadingText="loadingText"
-      class="col-span-2">
-      <template v-slot:action>
-        <create-field></create-field>
-      </template>
-      <template v-slot:content>
-        <list-fields :clientSlug="client" :contractName="contractName"></list-fields>
-      </template>
-    </panel-template>
-    <component :is="updateComponent" :field="currentField"></component>
-    <panel-template class="col-span-2" title="Injected Fields" :showLoader="loading" loadingText="Loading Injected Fields">
-      <template v-slot:action>
-        <button class="btn btn-turquoise" @click="showCreateInjectedFieldModal()"><font-awesome-icon icon="plus"></font-awesome-icon> Add Injected Field</button>
-      </template>
-      <template v-slot:content>
-        <list-injected-fields></list-injected-fields>
-      </template>
-    </panel-template>
-    <update-injected-field></update-injected-field>
-    <create-injected-field></create-injected-field>
+    <fields-panel-template
+      :loading="loading"
+      :loading-text="loadingText"
+      :contract-name="contractName"
+      :client-slug="client"
+      class="col-span-2"/>
+    <injected-panel-template
+      :loading="loading"
+      class="col-span-2"/>
   </base-panel-grid>
 </template>
 
 <script>
 import { mapGetters, mapMutations, mapActions } from 'vuex'
-import listFields from '@/components/forms/fields/list'
-import createField from '@/components/forms/fields/create'
-import createInjectedField from '@/components/forms/injected-fields/fields/create'
-import updateTextField from '@/components/forms/fields/text-fields/update'
-import updateOptionField from '@/components/forms/fields/option-fields/update'
-import listInjectedFields from '@/components/forms/injected-fields/fields/list'
-import updateInjectedField from '@/components/forms/injected-fields/fields/update'
+import fieldsPanel from '@/components/forms/fields/panel-template'
+import injectedFieldsPanel from '@/components/forms/injected-fields/fields/panel-template'
 
 export default {
   props: {
     client: String,
     id: Number
   },
-  data () {
-    return {
-      currentFieldId: {
-        type: Number
-      },
-      updateComponent: null
-    }
-  },
   computed: {
     ...mapGetters({
-      currentField: 'getCurrentField',
       fetchFormsLoading: 'getFetchFormsLoading',
       fetchFormsLoadingText: 'getFetchFormsLoadingText',
       offerFetchLoading: 'getOfferFetchLoading',
@@ -61,7 +32,6 @@ export default {
       buyerFetchLoadingText: 'getBuyerFetchLoadingText',
       getCurrentOffer: 'getCurrentOffer',
       getCurrentBuyer: 'getCurrentBuyer',
-      injectedFieldTypes: 'getInjectedFieldTypes'
     }),
     contractType () {
       return this.$route.name === 'OfferFieldManagement' ? 'offer' : 'buyer'
@@ -90,45 +60,13 @@ export default {
   },
   methods: {
     ...mapMutations({
-      showUpdateTextFieldModal: 'SHOW_UPDATE_TEXT_FIELD_MODAL',
-      showUpdateOptionFieldModal: 'SHOW_UPDATE_OPTION_FIELD_MODAL',
-      resetAncestorForms: 'RESET_ANCESTOR_FORMS',
-      showCreateInjectedFieldModal: 'SHOW_CREATE_INJECTED_FIELD_MODAL'
+      resetAncestorForms: 'RESET_ANCESTOR_FORMS'
     }),
     ...mapActions({
-      fetchForms: 'fetchForms',
-      fetchInjectedFieldTypes: 'fetchInjectedFieldTypes'
-    }),
-    async setUpdateComponent (component) {
-      this.updateComponent = component
-    }
-  },
-  components: {
-    updateTextField,
-    updateOptionField,
-    'list-fields': listFields,
-    'create-field': createField,
-    'create-injected-field': createInjectedField,
-    'list-injected-fields': listInjectedFields,
-    'update-injected-field': updateInjectedField
+      fetchForms: 'fetchForms'
+    })
   },
   watch: {
-    currentField () {
-      if (this.currentField) {
-        this.currentFieldId = this.currentField.id
-        if (this.currentField.type === 'select' || this.currentField.type === 'radio') {
-          this.setUpdateComponent(updateOptionField).then(() => {
-            this.showUpdateOptionFieldModal()
-          })
-        } else if (this.currentField.type === 'text' || this.currentField.type === 'textarea') {
-          this.setUpdateComponent(updateTextField).then(() => {
-            this.showUpdateTextFieldModal()
-          })
-        }
-      } else {
-        this.currentFieldId = null
-      }
-    },
     id () { // id prop changes when navigating from a buyer to an ancestor buyer
       this.resetAncestorForms()
       this.fetchForms()
@@ -137,7 +75,10 @@ export default {
   created () {
     this.resetAncestorForms()
     this.fetchForms()
-    this.fetchInjectedFieldTypes()
+  },
+  components: {
+    'fields-panel-template': fieldsPanel,
+    'injected-panel-template': injectedFieldsPanel
   }
 }
 </script>
