20ede187c09422de86a8ac80d151de3e8896e151
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-09 16:31:57 -0600

    styling the list

--- src/components/forms/additional-content/list.vue
+++ src/components/forms/additional-content/list.vue
@@ -1,10 +1,24 @@
 <template>
-  <ul>
-    <li class="card" v-for="content in form.additional_form_content_tcpa" :key="`additionalContent-${content.id}`">
-      <h4 class="h4">{{ content.type }}</h4>
-      <text-field :field_disabled="true"></text-field>
-    </li>
-  </ul>
+  <div class="space-y-2">
+    <panel-template
+      v-for="content in form.additional_form_content_tcpa"
+      :key="`additionalContent-${content.id}`"
+      :title="content.additional_content_type"
+      contentBackgroundColor="white"
+    >
+      <template #content>
+        <div class="space-x-4 mb-3">
+          <span v-if="content.additional_content_type === 'tcpa'">
+            Leadid Toggle: <span class="font-bold" :class="content.leadid_toggle ? 'text-green-600' : 'text-red-500'">{{ content.leadid_toggle }}</span>
+          </span>
+          <span>
+            Double Opt-in: <span class="font-bold" :class="content.double_optin ? 'text-green-600' : 'text-red-500'">{{ content.double_optin }}</span>
+          </span>
+        </div>
+        <div class="well">{{ content.additional_content_block }}</div>
+      </template>
+    </panel-template>
+  </div>
 </template>
 
 <script>
