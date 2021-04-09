ab215db93bf6c7d66eab8ce5d7a4b3fb5eeb609a
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-09 16:40:53 -0600

    breaking list item out into its own component

---
+++ src/components/forms/additional-content/list-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <panel-template
+    :title="content.additional_content_type"
+    contentBackgroundColor="white"
+  >
+    <template #content>
+      <div class="space-x-4 mb-3">
+          <span v-if="content.additional_content_type === 'tcpa'">
+            Leadid Toggle: <span class="font-bold" :class="content.leadid_toggle ? 'text-green-600' : 'text-red-500'">{{ content.leadid_toggle }}</span>
+          </span>
+        <span>
+            Double Opt-in: <span class="font-bold" :class="content.double_optin ? 'text-green-600' : 'text-red-500'">{{ content.double_optin }}</span>
+          </span>
+      </div>
+      <div class="well">{{ content.additional_content_block }}</div>
+    </template>
+  </panel-template>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/components/forms/additional-content/list.vue
+++ src/components/forms/additional-content/list.vue
@@ -1,30 +1,21 @@
 <template>
   <div class="space-y-2">
-    <panel-template
+    <list-item
       v-for="content in form.additional_form_content_tcpa"
       :key="`additionalContent-${content.id}`"
-      :title="content.additional_content_type"
-      contentBackgroundColor="white"
-    >
-      <template #content>
-        <div class="space-x-4 mb-3">
-          <span v-if="content.additional_content_type === 'tcpa'">
-            Leadid Toggle: <span class="font-bold" :class="content.leadid_toggle ? 'text-green-600' : 'text-red-500'">{{ content.leadid_toggle }}</span>
-          </span>
-          <span>
-            Double Opt-in: <span class="font-bold" :class="content.double_optin ? 'text-green-600' : 'text-red-500'">{{ content.double_optin }}</span>
-          </span>
-        </div>
-        <div class="well">{{ content.additional_content_block }}</div>
-      </template>
-    </panel-template>
+      :content="content"
+    />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import listItem from './list-item'
 
 export default {
+  components: {
+    'list-item': listItem
+  },
   computed: {
     ...mapGetters({
       form: 'getCurrentForm'
