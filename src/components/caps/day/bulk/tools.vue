0654548952e810516a30d167dc0c765986026924
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-20 11:36:14 -0600

    tidying up the bulk edit tools component

--- src/components/caps/day/bulk/tools.vue
+++ src/components/caps/day/bulk/tools.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="p-1">
-    <div class="flex flex-row justify-between items-center border-b-2 border-gray-200 p-2 pb-5">
+  <div class="p-3">
+    <div class="flex flex-row justify-between items-center pb-1">
       <span>
         <ul class="flex flex-row justify-center space-x-2">
-          <li v-for="filter in massFilters" :key="filter.name" class="flex flex-col items-center">
+          <li v-for="filter in massFilters" :key="filter.name" class="flex flex-row items-center space-x-2 well">
             <span class="capitalize font-bold">{{ filter.name }}</span>
             <span>
-              <button class="btn btn-md btn-blue rounded-r-none border-r-0 w-10" @click="removeDates(formatDates(filter.days))"><font-awesome-icon :icon="['far', 'square']"></font-awesome-icon></button>
-              <button class="btn btn-md btn-blue rounded-l-none w-10" @click="addDays(formatDates(filter.days))"><font-awesome-icon icon="check-square"></font-awesome-icon></button>
+              <button class="btn btn-md btn-blue rounded-r-none border-r-0 w-12" @click="removeDates(formatDates(filter.days))"><font-awesome-icon :icon="['far', 'square']"></font-awesome-icon></button>
+              <button class="btn btn-md btn-blue rounded-l-none w-12" @click="addDays(formatDates(filter.days))"><font-awesome-icon icon="check-square"></font-awesome-icon></button>
             </span>
           </li>
         </ul>
       </span>
-      <button class="btn btn-turquoise rounded-full" @click="showModal()">Update Selected Day Caps</button>
+      <button class="btn btn-turquoise rounded-full" @click="showModal()" :disabled="selectedDates.length === 0">Update Selected Day Caps</button>
     </div>
-    <ul class="grid grid-cols-7 gap-2 justify-center p-2 pb-0 mt-3">
+    <ul class="grid grid-cols-7 gap-2 justify-center pb-0 mt-3">
       <li v-for="filter in dayFilters" :key="filter.name" class="flex flex-row flex-grow items-center">
         <button class="w-1/2 btn btn-md btn-blue rounded-r-none border-r-0" @click="removeDates(formatDates(filter.days))"><font-awesome-icon :icon="['far', 'square']"></font-awesome-icon></button>
         <button class="w-1/2 btn btn-md btn-blue rounded-l-none" @click="addDays(formatDates(filter.days))"><font-awesome-icon icon="check-square"></font-awesome-icon></button>
@@ -25,7 +25,7 @@
 
 <script>
 import dayjs from 'dayjs'
-import { mapMutations } from 'vuex'
+import { mapMutations, mapGetters } from 'vuex'
 
 const currentMonth = dayjs(new Date()).month() + 1
 const currentDay = dayjs(new Date()).date()
@@ -36,6 +36,9 @@ export default {
     }
   },
   computed: {
+    ...mapGetters({
+      selectedDates: 'getBulkUpdateDayCaps'
+    }),
     weekdays () { return this.days.filter(day => [2, 3, 4, 5, 6].includes(day.weekday)) },
     weekends () { return this.days.filter(day => [1, 7].includes(day.weekday)) },
     sundays () { return this.days.filter(day => [1].includes(day.weekday)) },
