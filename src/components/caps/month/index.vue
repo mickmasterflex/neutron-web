92cb6d90cbac1e8bb2715ee1d25c145504df2ce5
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-20 14:22:50 -0600

    disabling edit/create of past month cap

--- src/components/caps/month/index.vue
+++ src/components/caps/month/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <calendar-header :calendarRefs="calendarRefs" :calendarData="calendarData" :headerClass="`${hasCap() ? monthCardColor(month) :'card-gray'}`">
+  <calendar-header :calendarRefs="calendarRefs" :calendarData="calendarData" :headerClass="`${hasCap? monthCardColor(month) :'card-gray'}`">
     <template v-slot:header-right>
       <div class="flex flex-row" v-if="month">
         <span class="mr-3 text-gray-700">
           <span class="card-colored-text font-bold">Sold: </span><span class="text-3xl">{{ month.sold }}</span>
         </span>
         <span v-if="month.id">
-          <span class="card-colored-text font-bold">Cap: </span> <span @click="updateCap()" class="text-link text-3xl">{{ Number(month.limit).toLocaleString() }}</span>
+          <span class="card-colored-text font-bold">Cap: </span>
+          <span @click="updateCap()" class="text-link text-3xl" v-if="currentOrFutureMonth">{{ Number(month.limit).toLocaleString() }}</span>
+          <span class="text-3xl" v-else>{{ Number(month.limit).toLocaleString() }}</span>
         </span>
-        <button v-else class="btn btn-turquoise rounded-full" @click="addCap()"><font-awesome-icon icon="plus"></font-awesome-icon> Add Month Cap</button>
+        <button v-else-if="currentOrFutureMonth" class="btn btn-turquoise rounded-full" @click="addCap()"><font-awesome-icon icon="plus"></font-awesome-icon> Add Month Cap</button>
       </div>
     </template>
   </calendar-header>
@@ -18,6 +20,7 @@
 import { mapGetters, mapMutations } from 'vuex'
 import { cardColor } from '@/mixins/card-color'
 import fullCalendarHeader from '@/components/ui/calendars/full-calendar/header'
+import dayjs from 'dayjs'
 
 export default {
   props: {
@@ -39,11 +42,6 @@ export default {
       this.setSelectedCapMonth(this.month)
       this.showUpdate()
     },
-    hasCap () {
-      if (this.month) {
-        return this.month.id !== null
-      }
-    },
     monthCardColor (attributes) {
       return this.cardColor(attributes.limit, attributes.sold)
     }
@@ -51,7 +49,16 @@ export default {
   computed: {
     ...mapGetters({
       month: 'getMonthCapByCurrentMonthDate'
-    })
+    }),
+    hasCap () {
+      const id = this.month ? this.month.id : null
+      return id !== null
+    },
+    currentOrFutureMonth () {
+      const today = dayjs(new Date())
+      const month = dayjs(this.month.date)
+      return month.$M >= today.$M
+    }
   },
   components: {
     'calendar-header': fullCalendarHeader
