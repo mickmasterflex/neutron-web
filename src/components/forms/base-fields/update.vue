fb3cbc58888f952df08773bf494bde0ab4363393
Kevin Bigelow <kbigelow5@gmail.com>
2021-04-23 10:28:38 -0600

    fixin up base option field save

--- src/components/forms/base-fields/update.vue
+++ src/components/forms/base-fields/update.vue
@@ -9,10 +9,10 @@
           <textarea-field v-model="description" field_id="description" field_label="Description"></textarea-field>
           <v-select-field v-model="type" :options="formatListForSelectOptions(typeOptions)" rules="required" field_id="type" field_label="Type"></v-select-field>
         </form>
-        <div v-if="optionFieldSelected && optionFieldOptions.length" class="border-t mt-6">
-          <field-options></field-options>
-        </div>
       </validation-observer>
+      <div v-if="optionFieldSelected" class="border-t mt-6">
+        <field-options></field-options>
+      </div>
     </template>
     <template v-slot:footer-additional>
       <button @click="submitForm()" class="btn btn-green btn-lg" :disabled="loading">
@@ -116,6 +116,7 @@ export default {
     }),
     async updateField (data) {
       if (this.optionFieldSelected) {
+        await this.updateOptions()
         await this.updateBaseOptionField(data)
       } else if (this.textFieldSelected) {
         await this.updateBaseTextField(data)
@@ -138,9 +139,6 @@ export default {
     submitForm () {
       this.$refs.form.validate().then(success => {
         if (success) {
-          if (this.optionFieldSelected) {
-            this.updateOptions()
-          }
           this.updateField({
             name: this.name,
             label: this.label,
