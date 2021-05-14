b42844f5bcd857f0b134f0fb4afde30f583e7e95
Kevin Bigelow <kbigelow5@gmail.com>
2021-05-14 15:37:56 -0600

    printing contract name in modal header

--- src/components/caps/modal/modal.vue
+++ src/components/caps/modal/modal.vue
@@ -1,6 +1,6 @@
 <template>
   <modal-template :show="showModal" @close="close" modalClass="modal-lg">
-    <template v-slot:header>Update Caps</template>
+    <template v-slot:header>{{ capParent ? capParent.name : '' }} Caps</template>
     <template v-slot:body>
       <lead-caps class="relative"/>
     </template>
@@ -15,8 +15,24 @@ import { mapGetters, mapMutations, mapActions } from 'vuex'
 export default {
   computed: {
     ...mapGetters({
-      showModal: 'getShowCapsModal'
-    })
+      showModal: 'getShowCapsModal',
+      buyerById: 'getBuyerById',
+      partnerById: 'getPartnerById',
+      buyerByCapParentId: 'getBuyerByCapParentId',
+      partnerByCapParentId: 'getPartnerByCapParentId',
+      offerByCapParentId: 'getOfferByCapParentId',
+      capsParentId: 'getCurrentCapsParentId',
+      capsParentType: 'getCurrentCapsParentType'
+    }),
+    capParent () {
+      const updatedParentOptions = {
+        buyers: this.buyerByCapParentId(this.capsParentId),
+        partners: this.partnerByCapParentId(this.capsParentId),
+        'offer-contracts': this.offerByCapParentId(this.capsParentId),
+        relations: { name: 'Relation' }
+      }
+      return updatedParentOptions[this.capsParentType]
+    }
   },
   methods: {
     ...mapMutations({
