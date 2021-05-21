706368ce423a810fbe8573895816f58dc3516434
Kevin Bigelow <kbigelow5@gmail.com>
2021-05-21 16:34:33 -0600

    general readability updates

--- src/components/caps/td-preview/caps-count.vue
+++ src/components/caps/td-preview/caps-count.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="flex flex-row space-x-1">
-    <date-display class="rounded bg-gray-100" :heading="thisMonthDisplay" :content="findCap(monthCaps, currentMonthDate)"/>
-    <date-display :heading="displayDay(dayOneDate)" :content="findCap(dayCaps, dayOneDate)"/>
-    <date-display class="hidden lg:flex" :heading="displayDay(dayTwoDate)" :content="findCap(dayCaps, dayTwoDate)"/>
-    <date-display class="hidden lg:flex" :heading="displayDay(dayThreeDate)" :content="findCap(dayCaps, dayThreeDate)"/>
-    <date-display class="hidden lg:flex" :heading="displayDay(dayFourDate)" :content="findCap(dayCaps, dayFourDate)"/>
-    <date-display class="hidden xl:flex" :heading="displayDay(dayFiveDate)" :content="findCap(dayCaps, dayFiveDate)"/>
-    <date-display class="hidden xl:flex" :heading="displayDay(daySixDate)" :content="findCap(dayCaps, daySixDate)"/>
-    <date-display class="hidden xl:flex" :heading="displayDay(daySevenDate)" :content="findCap(dayCaps, daySevenDate)"/>
+    <date-display class="border-r border-gray-200"
+                  :heading="today.format('MMM')"
+                  :limit="capLimit(monthCaps, currentMonthDate)"/>
+    <date-display :heading="dayHeading(dayOneDate)"
+                  :limit="capLimit(dayCaps, dayOneDate)"/>
+    <date-display class="hidden lg:flex"
+                  :heading="dayHeading(dayTwoDate)"
+                  :limit="capLimit(dayCaps, dayTwoDate)"/>
+    <date-display class="hidden lg:flex"
+                  :heading="dayHeading(dayThreeDate)"
+                  :limit="capLimit(dayCaps, dayThreeDate)"/>
+    <date-display class="hidden lg:flex"
+                  :heading="dayHeading(dayFourDate)"
+                  :limit="capLimit(dayCaps, dayFourDate)"/>
+    <date-display class="hidden xl:flex"
+                  :heading="dayHeading(dayFiveDate)"
+                  :limit="capLimit(dayCaps, dayFiveDate)"/>
+    <date-display class="hidden xl:flex"
+                  :heading="dayHeading(daySixDate)"
+                  :limit="capLimit(dayCaps, daySixDate)"/>
+    <date-display class="hidden xl:flex"
+                  :heading="dayHeading(daySevenDate)"
+                  :limit="capLimit(dayCaps, daySevenDate)"/>
   </div>
 </template>
 
@@ -21,22 +36,6 @@ export default {
   props: {
     caps: Object
   },
-  methods: {
-    displayDay (date) {
-      return dayjs(date).format('Do')
-    },
-    formatDate (date) {
-      return date.format('YYYY-MM-DD')
-    },
-    findCap (caps, date) {
-      const cap = caps.find(cap => cap.date === date)
-      if (cap) {
-        return cap.limit
-      } else {
-        return '-'
-      }
-    }
-  },
   computed: {
     today () {
       return dayjs()
@@ -49,9 +48,6 @@ export default {
     },
     currentMonthDate () {
       return this.today.format('YYYY-MM') + '-01'
-    },
-    thisMonthDisplay () {
-      return this.today.format('MMM')
     },
     dayOneDate () {
       return this.formatDate(this.today)
@@ -73,6 +69,22 @@ export default {
     },
     daySevenDate () {
       return this.formatDate(this.today.add(6, 'day'))
+    }
+  },
+  methods: {
+    dayHeading (date) {
+      return dayjs(date).format('Do')
+    },
+    formatDate (date) {
+      return date.format('YYYY-MM-DD')
+    },
+    capLimit (caps, date) {
+      const cap = caps.find(cap => cap.date === date)
+      if (cap) {
+        return cap.limit
+      } else {
+        return null
+      }
     }
   },
   components: {
--- src/components/caps/td-preview/date.vue
+++ src/components/caps/td-preview/date.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col flex-grow py-1 px-2">
     <span class="text-gray-700">{{ heading }}</span>
-    <span class="font-bold text-gray-900 px-1">{{ content }}</span>
+    <span class="font-bold text-gray-900 px-1">
+      {{ limit ? limit : '-' }}
+    </span>
   </div>
 </template>
 
@@ -12,8 +14,8 @@ export default {
       type: String,
       required: true
     },
-    content: {
-      required: true
+    limit: {
+      type: Number
     }
   }
 }
