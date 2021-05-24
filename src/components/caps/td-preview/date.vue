c5abe5102efebd14eec76b8276533f8aa556cc98
Kevin Bigelow <kbigelow5@gmail.com>
2021-05-24 14:16:23 -0600

    adding commas to cap limit

--- src/components/caps/td-preview/date.vue
+++ src/components/caps/td-preview/date.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col flex-grow py-1 px-2">
     <span class="text-gray-700">{{ heading }}</span>
     <span class="font-bold text-gray-900">
-      {{ limit ? limit : '-' }}
+      {{ limit ? limit.toLocaleString() : '-' }}
     </span>
   </div>
 </template>
