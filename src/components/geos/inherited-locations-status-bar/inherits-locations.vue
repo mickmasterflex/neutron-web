e61a0a72b5699dfbf27582e50c2e24e096ee62e9
Kevin Bigelow <kbigelow5@gmail.com>
2021-06-02 13:45:37 -0600

    fixing space

--- src/components/geos/inherited-locations-status-bar/inherits-locations.vue
+++ src/components/geos/inherited-locations-status-bar/inherits-locations.vue
@@ -6,8 +6,7 @@
         class="underline mr-2"
         :key="`geoInheritedContract-${contract.contract_id}`"
         v-for="contract in inheritedLocationContracts"
-        :to="{ name: 'BuyerContract', params: { id: contract.contract_id, client: currentClientData.slug } }">
-        {{ contract.contract_name }}</router-link>
+        :to="{ name: 'BuyerContract', params: { id: contract.contract_id, client: currentClientData.slug } }">{{ contract.contract_name }}</router-link>
     </template>
   </status-bar>
 </template>
