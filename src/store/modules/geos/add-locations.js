4229438c4e6a34b2e901c495486e750f85453562
Emily Jonaitis <emilyfjonaitis@gmail.com>
2021-06-07 15:09:55 -0600

    success of add locations

--- src/store/modules/geos/add-locations.js
+++ src/store/modules/geos/add-locations.js
@@ -1,10 +1,13 @@
 import axios from '@/axios'
+import { successfulToast } from '@/mixins/toast-messages'
 
 const actions = {
-  async addLocations ({ commit }, location) {
+  async addLocations ({ commit }, location, getters) {
     commit('SET_GEO_PANEL_LOADING')
-    await axios.post('/geo-add-locations/', location)
-      .finally(() => {
+    await axios.post('/geo-add-locations/', location, { showSuccessToast: false })
+      .then(response => {
+        successfulToast({ heading: 'Valid Locations', content: response.data.valid_locations })
+      }).finally(() => {
         commit('RESET_GEO_PANEL_LOADING')
       })
   }
