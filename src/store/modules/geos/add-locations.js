f442506a0752715b5262284c6ddb8ab53771f7e4
Emily Jonaitis <emilyfjonaitis@gmail.com>
2021-06-08 11:54:07 -0600

    no toast message for null

--- src/store/modules/geos/add-locations.js
+++ src/store/modules/geos/add-locations.js
@@ -1,18 +1,23 @@
 import axios from '@/axios'
-import { successfulToast } from '@/mixins/toast-messages'
+import { successfulToast, failedToast } from '@/mixins/toast-messages'
 
 const actions = {
-  async addLocations ({ commit }, location, getters) {
+  async addLocations ({ commit }, location) {
     commit('SET_GEO_PANEL_LOADING')
     await axios.post('/geo-add-locations/', location, { showSuccessToast: false })
       .then(response => {
-        successfulToast({ heading: 'Valid Locations', content: response.data.valid_locations })
+        if (!response.data.valid_locations) {
+          successfulToast({ heading: 'Valid Locations', content: response.data.valid_locations })
+        }
+        if (!response.data.invalid_locations) {
+          failedToast({ heading: 'Invalid Location', content: response.data.invalid_locations })
+        }
+        successfulToast({ heading: 'Inherited Locations', content: response.data.inherited_locations })
       }).finally(() => {
         commit('RESET_GEO_PANEL_LOADING')
       })
   }
 }
-
 export default {
   actions
 }
