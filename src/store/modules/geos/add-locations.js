45eadf33768b2a3989da8bd5132870c8714a6ce5
Emily Jonaitis <emilyfjonaitis@gmail.com>
2021-06-08 13:28:41 -0600

    if greater than 0 show toast messege

--- src/store/modules/geos/add-locations.js
+++ src/store/modules/geos/add-locations.js
@@ -6,13 +6,15 @@ const actions = {
     commit('SET_GEO_PANEL_LOADING')
     await axios.post('/geo-add-locations/', location, { showSuccessToast: false })
       .then(response => {
-        if (!response.data.valid_locations) {
+        if (response.data.valid_locations.length > 0) {
           successfulToast({ heading: 'Valid Locations', content: response.data.valid_locations })
         }
-        if (!response.data.invalid_locations) {
+        if (response.data.invalid_locationslength > 0) {
           failedToast({ heading: 'Invalid Location', content: response.data.invalid_locations })
         }
-        successfulToast({ heading: 'Inherited Locations', content: response.data.inherited_locations })
+        if (response.data.inherited_locationslength > 0) {
+          failedToast({ heading: 'Location already Inherited', content: response.data.inherited_locations })
+        }
       }).finally(() => {
         commit('RESET_GEO_PANEL_LOADING')
       })
