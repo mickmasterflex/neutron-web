ba7f408b758cb33c80a71897e3d9ab8a9e8c3555
Emily Jonaitis <emilyfjonaitis@gmail.com>
2021-06-10 10:04:18 -0600

    removing useless stupid getters and mutations

--- src/store/modules/verticals/loading.js
+++ src/store/modules/verticals/loading.js
@@ -4,13 +4,10 @@ const state = {
 }
 
 const getters = {
-  getVerticalsLoading: state => state.verticals_loading,
   getVerticalsFetchLoading: state => state.verticals_fetch_loading
 }
 
 const mutations = {
-  SET_VERTICALS_LOADING: (state) => (state.verticals_loading = true),
-  RESET_VERTICALS_LOADING: (state) => (state.verticals_loading = false),
   SET_VERTICALS_FETCH_LOADING: (state) => (state.verticals_fetch_loading = true),
   RESET_VERTICALS_FETCH_LOADING: (state) => (state.verticals_fetch_loading = false)
 }
